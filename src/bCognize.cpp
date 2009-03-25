ba3595f75267e163fb7ca791d8625ec21114afc5
wiebel <devnull@localhost>
2009-03-25 12:33:37 +0000

    PREPARATION PHASE: Added a test executable and more instructions.

---
+++ src/bCognize.cpp
@@ -0,0 +1,15 @@
+/*
+ * bCognize.cc
+ *
+ *  Created on: Mar 25, 2009
+ *      Author: wiebel
+ */
+
+#include <iostream>
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+	cout << "To start the new project there has to be this main." << endl;
+	return 0;
+}
