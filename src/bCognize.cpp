2272c76df55e235fa9aa2659bc92fd27516865ee
wiebel <devnull@localhost>
2009-03-30 18:01:52 +0000

    PREPARATION PHASE: made some dummy window appear

--- src/bCognize.cpp
+++ src/bCognize.cpp
@@ -1,15 +1,27 @@
 /*
- * bCognize.cc
+ * bCognize.cpp
  *
  *  Created on: Mar 25, 2009
  *      Author: wiebel
  */
 
 #include <iostream>
+#include <QtGui/QApplication>
+
+#include "gui/qt4/BCMainWindow.h"
 using namespace std;
 
 int main( int argc, char* argv[] )
 {
 	cout << "To start the new project there has to be this main." << endl;
-	return 0;
+    QApplication appl( argc, argv );
+    QMainWindow *widget = new QMainWindow;
+    BCMainWindow gui;
+    gui.setupGUI(widget);
+
+
+    widget->show();
+	int qtExecResult;
+    qtExecResult = appl.exec ();
+	return qtExecResult;
 }
---
+++ src/gui/icons/icons.h
@@ -0,0 +1,4 @@
+#include "logoIcon.xpm"
+#include "disc.xpm"
+#include "fileopen.xpm"
+#include "quit.xpm"
