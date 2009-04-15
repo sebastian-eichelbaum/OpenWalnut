13e454dfb98f8dd573a7f547cf506bf0e0236ab3
wiebel <devnull@localhost>
2009-04-15 14:35:59 +0000

    PREPARATION PHASE: Removed useless cout, added subdirectory io and added the eclipse definition file fo the coding style

--- src/bCognize.cpp
+++ src/bCognize.cpp
@@ -13,7 +13,6 @@ using namespace std;
 
 int main( int argc, char* argv[] )
 {
-	cout << "To start the new project there has to be this main." << endl;
     QApplication appl( argc, argv );
     QMainWindow *widget = new QMainWindow;
     BCMainWindow gui;
