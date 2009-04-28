20df1599a96b1714c5f1913166b47a87f9b8e52f
wiebel <devnull@localhost>
2009-04-28 13:18:14 +0000

    changed inlclude from gl to glew

--- src/graphicsEngine/BGLScenePainter.cpp
+++ src/graphicsEngine/BGLScenePainter.cpp
@@ -5,8 +5,7 @@
 //---------------------------------------------------------------------------
 
 #include <cassert>
-#include <GL/gl.h>
-#include <GL/glu.h>
+#include <GL/glew.h>
 
 #include "BGLScenePainter.h"
 
