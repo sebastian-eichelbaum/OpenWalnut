da2bf21473e77dfd0f89822c00f9c97085237512
wiebel <devnull@localhost>
2009-04-28 15:47:29 +0000

    changed comment_header for LGPL

--- src/gui/icons/BIcons.h
+++ src/gui/icons/BIcons.h
@@ -2,7 +2,25 @@
 //
 // Project: BrainCognize
 //
+// Copyright 2009 SomeCopyrightowner
+//
+// This file is part of BrainCognize.
+//
+// BrainCognize is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BrainCognize is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BrainCognize. If not, see <http://www.gnu.org/licenses/>.
+//
 //---------------------------------------------------------------------------
+
 #ifndef BICONS_H
 #define BICONS_H
 
