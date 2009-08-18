d9fd6def0d33ccbd23447865a3e5fd3d72cfd2be
wiebel <devnull@localhost>
2009-08-18 15:53:58 +0000

    [ADD] added file to be bale to comment namespace wmath a sensible place

---
+++ src/math/WMath.h
@@ -0,0 +1,41 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut
+//
+// Copyright 2009 SomeCopyrightowner
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WMATH_H
+#define WMATH_H
+
+/**
+ * All classes and functions of math module of OpenWalnut.
+ */
+namespace wmath
+{
+// wiebel:
+//   There is no code here yet, because I created this file only
+//   to have a good place to specify the documentation for the
+//   namespace. There are already classes belonging to this
+//   namespace in other files.
+//
+//   Please remove this comment if it is not appropriate anymore.
+}
+
+#endif  // WMATH_H
