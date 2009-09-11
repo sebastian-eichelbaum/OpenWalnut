844a0808aca85efcfbbc087e94ff774f002027cf
Alexander Wiebel <openwalnut@dergrosse.de>
2009-09-11 14:56:33 +0200

    [ADD] Loader, test and fixture for ASCII EEG data from ASA

---
+++ src/dataHandler/io/WLoaderEEGASCII.h
@@ -0,0 +1,57 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut
+//
+// Copyright 2009 SomeCopyrightowner
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WLOADEREEGASCII_H
+#define WLOADEREEGASCII_H
+
+
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include "../WDataHandler.h"
+#include "../WLoader.h"
+
+/**
+ * Loader for EEG data in ASCII fromat.
+ * \ingroup dataHandler
+ */
+class WLoaderEEGASCII : public WLoader
+{
+public:
+    /**
+     * Constructs a loader to be executed in its own thread and sets the data needed
+     * for the loader when executed in its own thread.
+     */
+    WLoaderEEGASCII( std::string fileName, boost::shared_ptr< WDataHandler > dataHandler );
+
+    /**
+     * This  function is automatically called when creating a new thread for the
+     * loader with boost::thread.
+     */
+    void operator()();
+
+protected:
+private:
+    boost::shared_ptr< WDataHandler > m_dataHandler;
+};
+
+#endif  // WLOADEREEGASCII_H
---
+++ src/dataHandler/io/test/WLoaderEEGASCII_test.h
@@ -0,0 +1,58 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut
+//
+// Copyright 2009 SomeCopyrightowner
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WLOADEREEGASCII_TEST_H
+#define WLOADEREEGASCII_TEST_H
+
+#include <string>
+#include <cxxtest/TestSuite.h>
+#include <boost/thread.hpp>
+
+#include "../WLoaderEEGASCII.h"
+
+/**
+ * Tests for the loader of ASCII EEG files.
+ */
+class WLoaderEEGASCIITest : public CxxTest::TestSuite
+{
+public:
+    /**
+     * Test whether loading itself does not throw an error.
+     */
+    void testLoading( void )
+    {
+        std::string fileName = "../fixtures/eeg_testData.asc";
+        std::cout << std::endl << "Test loading of " << fileName << "." << std::endl;
+
+        boost::shared_ptr< WDataHandler > dataHandler =
+            boost::shared_ptr< WDataHandler >( new WDataHandler() );
+        TS_ASSERT_EQUALS( dataHandler->getNumberOfDataSets(), 0 );
+
+        WLoaderEEGASCII eegASCIILoader( fileName, dataHandler );
+        boost::thread loaderThread( eegASCIILoader );
+        sleep( 3 );
+        TS_ASSERT_EQUALS( dataHandler->getNumberOfDataSets(), 1 );
+    }
+};
+
+#endif  // WLOADEREEGASCII_TEST_H
