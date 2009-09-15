8f63545c8535a1d51f320889b5f6fe61910d3d9e
schurade <devnull@localhost>
2009-09-15 13:03:41 +0200

    [STYLE] forget to check this before

--- src/kernel/WNavigationSliceModule.cpp
+++ src/kernel/WNavigationSliceModule.cpp
@@ -81,63 +81,62 @@ void WNavigationSliceModule::createSlices()
     sliceGeode->addDrawable( sliceGeometry );
 
     osg::Vec3Array* sliceVertices = new osg::Vec3Array;
-    sliceVertices->push_back( osg::Vec3(0, 50, 0) );
-    sliceVertices->push_back( osg::Vec3(0, 50, 100) );
-    sliceVertices->push_back( osg::Vec3(100, 50, 100) );
-    sliceVertices->push_back( osg::Vec3(100, 50, 0) );
+    sliceVertices->push_back( osg::Vec3( 0, 50, 0 ) );
+    sliceVertices->push_back( osg::Vec3( 0, 50, 100 ) );
+    sliceVertices->push_back( osg::Vec3( 100, 50, 100 ) );
+    sliceVertices->push_back( osg::Vec3( 100, 50, 0 ) );
 
-    sliceVertices->push_back( osg::Vec3(50, 0, 0) );
-    sliceVertices->push_back( osg::Vec3(50, 0, 100) );
-    sliceVertices->push_back( osg::Vec3(50, 100, 100) );
-    sliceVertices->push_back( osg::Vec3(50, 100, 0) );
+    sliceVertices->push_back( osg::Vec3( 50, 0, 0 ) );
+    sliceVertices->push_back( osg::Vec3( 50, 0, 100 ) );
+    sliceVertices->push_back( osg::Vec3( 50, 100, 100 ) );
+    sliceVertices->push_back( osg::Vec3( 50, 100, 0 ) );
 
-    sliceVertices->push_back( osg::Vec3(0, 0, 50) );
-    sliceVertices->push_back( osg::Vec3(0, 100, 50) );
-    sliceVertices->push_back( osg::Vec3(100, 100, 50) );
-    sliceVertices->push_back( osg::Vec3(100, 0, 50) );
+    sliceVertices->push_back( osg::Vec3( 0, 0, 50 ) );
+    sliceVertices->push_back( osg::Vec3( 0, 100, 50 ) );
+    sliceVertices->push_back( osg::Vec3( 100, 100, 50 ) );
+    sliceVertices->push_back( osg::Vec3( 100, 0, 50 ) );
 
     sliceGeometry->setVertexArray( sliceVertices );
 
     osg::Vec3Array* texCoords = new osg::Vec3Array;
-    texCoords->push_back( osg::Vec3(0.0, 0.5, 0.0) );
-    texCoords->push_back( osg::Vec3(0.0, 0.5, 1.0) );
-    texCoords->push_back( osg::Vec3(1.0, 0.5, 1.0) );
-    texCoords->push_back( osg::Vec3(1.0, 0.5, 0.0) );
+    texCoords->push_back( osg::Vec3( 0.0, 0.5, 0.0 ) );
+    texCoords->push_back( osg::Vec3( 0.0, 0.5, 1.0 ) );
+    texCoords->push_back( osg::Vec3( 1.0, 0.5, 1.0 ) );
+    texCoords->push_back( osg::Vec3( 1.0, 0.5, 0.0 ) );
 
-    texCoords->push_back( osg::Vec3(0.5, 0.0, 0.0) );
-    texCoords->push_back( osg::Vec3(0.5, 0.0, 1.0) );
-    texCoords->push_back( osg::Vec3(0.5, 1.0, 1.0) );
-    texCoords->push_back( osg::Vec3(0.5, 1.0, 0.0) );
+    texCoords->push_back( osg::Vec3( 0.5, 0.0, 0.0 ) );
+    texCoords->push_back( osg::Vec3( 0.5, 0.0, 1.0 ) );
+    texCoords->push_back( osg::Vec3( 0.5, 1.0, 1.0 ) );
+    texCoords->push_back( osg::Vec3( 0.5, 1.0, 0.0 ) );
 
-    texCoords->push_back( osg::Vec3(0.0, 0.0, 0.5) );
-    texCoords->push_back( osg::Vec3(0.0, 1.0, 0.5) );
-    texCoords->push_back( osg::Vec3(1.0, 1.0, 0.5) );
-    texCoords->push_back( osg::Vec3(1.0, 0.0, 0.5) );
+    texCoords->push_back( osg::Vec3( 0.0, 0.0, 0.5 ) );
+    texCoords->push_back( osg::Vec3( 0.0, 1.0, 0.5 ) );
+    texCoords->push_back( osg::Vec3( 1.0, 1.0, 0.5 ) );
+    texCoords->push_back( osg::Vec3( 1.0, 0.0, 0.5 ) );
 
     sliceGeometry->setTexCoordArray( 0, texCoords );
 
-    osg::DrawElementsUInt* slice0 = new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
-    slice0->push_back(3);
-    slice0->push_back(2);
-    slice0->push_back(1);
-    slice0->push_back(0);
+    osg::DrawElementsUInt* slice0 = new osg::DrawElementsUInt( osg::PrimitiveSet::QUADS, 0 );
+    slice0->push_back( 3 );
+    slice0->push_back( 2 );
+    slice0->push_back( 1 );
+    slice0->push_back( 0 );
 
-    osg::DrawElementsUInt* slice1 = new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
-    slice1->push_back(7);
-    slice1->push_back(6);
-    slice1->push_back(5);
-    slice1->push_back(4);
+    osg::DrawElementsUInt* slice1 = new osg::DrawElementsUInt( osg::PrimitiveSet::QUADS, 0 );
+    slice1->push_back( 7 );
+    slice1->push_back( 6 );
+    slice1->push_back( 5 );
+    slice1->push_back( 4 );
 
-    osg::DrawElementsUInt* slice2 = new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
-    slice2->push_back(11);
-    slice2->push_back(10);
-    slice2->push_back(9);
-    slice2->push_back(8);
+    osg::DrawElementsUInt* slice2 = new osg::DrawElementsUInt( osg::PrimitiveSet::QUADS, 0 );
+    slice2->push_back( 11 );
+    slice2->push_back( 10 );
+    slice2->push_back( 9 );
+    slice2->push_back( 8 );
 
     sliceGeometry->addPrimitiveSet( slice0 );
     sliceGeometry->addPrimitiveSet( slice1 );
     sliceGeometry->addPrimitiveSet( slice2 );
 
     WKernel::getRunningKernel()->getGraphicsEngine()->getScene()->addChild( sliceGeode );
-
 }
