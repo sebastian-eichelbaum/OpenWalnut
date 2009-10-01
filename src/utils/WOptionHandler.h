642d9803efd33798b0afc140c2b3fdcc0e5c09e8
schurade <devnull@localhost>
2009-10-01 09:53:40 +0200

    [CODESTYLE] this rule has got to be the most stupid rule ever invented

--- src/utils/WOptionHandler.h
+++ src/utils/WOptionHandler.h
@@ -27,8 +27,8 @@
 
 #include <boost/program_options.hpp>
 
-//TODO(all): why is that include here?
-//#include "../gui/qt4/WMainApplication.h"
+// TODO(all): why is that include here?
+// #include "../gui/qt4/WMainApplication.h"
 
 // since the namespace is far to big we use a shortcut here:
 namespace po = boost::program_options;
