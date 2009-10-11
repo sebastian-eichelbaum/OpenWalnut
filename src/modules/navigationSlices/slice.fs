b7959e1ddee9f01a68d291295f729b78887451f9
Mario Hlawitschka <hlawitschka@ucdavis.edu>
2009-10-10 22:55:45 -0700

    [FIX] fixed shader

--- src/modules/navigationSlices/slice.fs
+++ src/modules/navigationSlices/slice.fs
@@ -4,7 +4,7 @@ uniform sampler3D tex0;
 
 void main()
 {
-    vec4 color = texture3D(tex0, VaryingTexCoord0);
+    vec4 color = texture3D(tex0, VaryingTexCoord0.xyz);
     if ( ( color.r + color.g + color.b ) < 0.01 )
         discard;
     gl_FragColor = color;
