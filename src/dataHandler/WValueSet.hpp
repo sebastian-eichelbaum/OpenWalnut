bd3797a5e57e888370cb1da849521b5433958f13
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2009-10-12 10:08:14 +0200

    [STYLE]

--- src/dataHandler/WValueSet.hpp
+++ src/dataHandler/WValueSet.hpp
@@ -60,16 +60,16 @@ public:
     {
         switch( m_order )
         {
-            case 0  : // scalar
+            case 0  :  // scalar
                       assert( m_dimension == 1 && "but m_order was 0" );
                       return rawSize();
-            case 1  : // vector
+            case 1  :  // vector
                       assert( rawSize() % m_dimension == 0 );
                       return rawSize() / m_dimension;
-            case 2  : // matrix
+            case 2  :  // matrix
                       assert( rawSize() % ( m_dimension * m_dimension ) == 0 );
                       return rawSize() / ( m_dimension * m_dimension );
-            default : // other
+            default :  // other
                       assert( 1 == 0 && "Unsupported tensor order" );
                       return 0;
         }
--- src/gui/qt4/signalslib.hpp
+++ src/gui/qt4/signalslib.hpp
@@ -1,8 +1,32 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV@Uni-Leipzig and CNCF@MPI-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
 #ifndef SIGNALSLIB_HPP_INCLUDED
 #define SIGNALSLIB_HPP_INCLUDED
 
 #if defined(signals) && defined(QOBJECTDEFS_H) && \
-  !defined(QT_MOC_CPP)
+  !defined( QT_MOC_CPP )
 #  undef signals
 #  define signals signals
 #endif
@@ -13,8 +37,8 @@ namespace boost
   namespace signalslib = signals;
 }
 
-#if defined(signals) && defined(QOBJECTDEFS_H) && \
-  !defined(QT_MOC_CPP)
+#if defined( signals ) && defined( QOBJECTDEFS_H ) && \
+  !defined( QT_MOC_CPP )
 #  undef signals
 // Restore the macro definition of "signals", as it was
 // defined by Qt's <qobjectdefs.h>.
