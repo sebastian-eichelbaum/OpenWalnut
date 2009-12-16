6771babf9a1cc1e96338b68834845eedfac529a6
Alexander Wiebel <wiebel_openwalnut@dergrosse.de>
2009-12-16 11:47:57 +0100

    [CHANGE #55] Made the shaders be copied optionally during every make

--- tools/CMakeModules/RemoveGeneratedTests.cmake
+++
@@ -1,7 +0,0 @@
-# this is a platform independent script for removing *_test.cc files:
-#--------------------------------------------------------------------
-
-FILE( GLOB_RECURSE GENERATED_TESTS ${CMAKE_CURRENT_BINARY_DIR}/*_test.cc )
-# we need a dummy in case of multiple invocation of the "make cleantest" target
-LIST( APPEND GENERATED_TESTS non_existing_dummy_file )
-FILE( REMOVE ${GENERATED_TESTS} )
---
+++ tools/CMakeModules/UpdateShaders.cmake
@@ -0,0 +1,15 @@
+# this is a script that puts all shaders to the right place or 
+# updates them if they are already there.
+#--------------------------------------------------------------------
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/bin/shaders)
+FILE( GLOB_RECURSE ALL_GE_SHADERS ${OW_SOURCE_DIR}/graphicsEngine/shaders/* )
+FOREACH( fname ${ALL_GE_SHADERS} )
+  #message( ${fname} )
+  configure_file(  ${fname} ${CMAKE_BINARY_DIR}/bin/shaders/ COPYONLY)
+ENDFOREACH( fname )
+FILE( GLOB_RECURSE ALL_MODULE_SHADERS ${OW_SOURCE_DIR}/modules/*/shaders/* )
+FOREACH( fname ${ALL_MODULE_SHADERS} )
+  #message( ${fname} )
+  configure_file(  ${fname} ${CMAKE_BINARY_DIR}/bin/shaders/ COPYONLY)
+ENDFOREACH( fname )
