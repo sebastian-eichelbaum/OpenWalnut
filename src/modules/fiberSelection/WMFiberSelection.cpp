0e51fd7a5c0b5dd6e776d9c2f2da30342a458a3b
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2010-03-05 16:23:42 +0100

    [MERGE]

--- src/modules/fiberSelection/WMFiberSelection.cpp
+++ src/modules/fiberSelection/WMFiberSelection.cpp
@@ -119,6 +119,8 @@ void WMFiberSelection::moduleMain()
     //
     m_moduleState.setResetable( true, true );
     m_moduleState.add( m_fibers->getDataChangedCondition() );
+    m_moduleState.add( m_voi1->getDataChangedCondition() );
+    m_moduleState.add( m_voi2->getDataChangedCondition() );
     m_moduleState.add( m_propCondition );
 
     // Signal ready state. Now your module can be connected by the container, which owns the module.
@@ -136,68 +138,10 @@ void WMFiberSelection::moduleMain()
             break;
         }
 
+
         bool dataChanged = false;
-/*       boost::shared_ptr< WDataSetSingle > newDataSet = m_input->getData();
-        bool dataChanged = ( m_dataSet != newDataSet );
-        if ( dataChanged || !m_dataSet )
-        // this condition will become true whenever the new data is different from the current one or our actual data is NULL. This handles all
-        // cases.
-        {
-            // The data is different. Copy it to our internal data variable:
-            debugLog() << "Received Data.";
-            m_dataSet = newDataSet;
-
-            // An output sent us an invalid dataset. You should always check this. You module most probably does not work on invalid pointers
-            // ;-).
-            if ( !m_dataSet )
-            {
-                debugLog() << "Invalid Data. Disabling.";
-
-                // In this situation it is the best to turn of the visualization from the last dataset and begin the loop from the beginning.
-                // This way you can ensure you always have valid data available.
-                continue;
-            }
-        }
-*/
-        // The input data has changed and is valid
-        if ( dataChanged )
-        {
-            debugLog() << "Data changed. Recalculating output.";
-/*
-            // Calculate your new data here. This example just forwards the input to the output ;-).
-            boost::shared_ptr< WDataSetSingle > newData = m_dataSet;
-
-            // Doing a lot of work without notifying the user visually is not a good idea. So how is it possible to report progress? Therefore,
-            // the WModule class provides a member m_progress which is of type WPropgressCombiner. You can create own progress objects and count
-            // them individually. The m_progress combiner provides this information to the GUI and the user.
-            // Here is a simple example:
-            int steps = 10;
-            boost::shared_ptr< WProgress > progress1 = boost::shared_ptr< WProgress >( new WProgress( "Doing work 1", steps ) );
-            m_progress->addSubProgress( progress1 );
-            for ( int i = 0; i < steps; ++i )
-            {
-                ++*progress1;
-                sleep( 1 );
-            }
-            progress1->finish();
-            // This creates a progress object with a name and a given number of steps. Your work loop can now increment the progress object. The
-            // progress combiner m_progress collects the progress and provides it to the GUI. When finished, the progress MUST be marked as
-            // finished using finish(). It is no problem to have several progress objects at the same time!
-
-            // Sometimes, the number of steps is not known. WProgress can also handle this. Simply leave away the last parameter (the number of
-            // steps. As with the other progress, you need to add it to the modules progress combiner and you need to mark it as finished with
-            // finish() if you are done with your work.
-            boost::shared_ptr< WProgress > progress2 = boost::shared_ptr< WProgress >( new WProgress( "Doing work 2" ) );
-            m_progress->addSubProgress( progress2 );
-            sleep( 2 );
-            progress2->finish();
-
-            // How to set the data to the output and how to notify other modules about it?
-            m_output->updateData( newData );
-            // This sets the new data to the output connector and automatically notifies all modules connected to your output.
-            */
-        }
     }
+
 }
 
 void WMFiberSelection::activate()
