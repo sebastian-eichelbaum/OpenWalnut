44432f35799bf85e9cd14c1c40b8a168cc72c962
Alexander Wiebel <wiebel_openwalnut@dergrosse.de>
2010-03-23 16:30:20 +0100

    [FIX] shader works now for me too

--- src/modules/triangleMeshRenderer/shaders/triangleMeshRenderer.fs
+++ src/modules/triangleMeshRenderer/shaders/triangleMeshRenderer.fs
@@ -1,5 +1,4 @@
 varying vec4 VaryingTexCoord0;
-attribute vec4 gl_Color;
 uniform int opacity;
 
 #include "lighting.fs"
@@ -11,11 +10,9 @@ void main()
     vec4 ambient = vec4(0.0);
     vec4 diffuse = vec4(0.0);
     vec4 specular = vec4(0.0);
-
     calculateLighting(-normal, gl_FrontMaterial.shininess, ambient, diffuse, specular);
 
     col = (ambient * col / 2.0) + (diffuse * col) + (specular * col / 3.0);
-
     col = clamp(col, 0.0, 1.0);
     col.a = opacity * .01;
 
