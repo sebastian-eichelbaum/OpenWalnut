518df6e95a79667ebede70ffebd424b12b002934
reichenbach <devnull@localhost>
2010-04-14 11:24:49 +0200

    [CHANGE] renamed WBinom.* to WCompileTimeFunctions.*

--- src/common/math/WCompileTimeFunctions.h
+++ src/common/math/WCompileTimeFunctions.h
@@ -22,5 +22,64 @@
 //
 //---------------------------------------------------------------------------
 
+#ifndef WCOMPILETIMEFUNCTIONS_H
+#define WCOMPILETIMEFUNCTIONS_H
 
-#include "WBinom.h"
+#include <string>
+
+namespace wmath
+{
+/**
+ * Implements compile-time calculation of binomial coefficients.
+ *
+ *
+ * WBinom< n, k >::value = n! / ( k!(n-k)! ).
+ *
+ * \note For k > n, compilation fails.
+ */
+template< std::size_t n, std::size_t k >
+struct WBinom
+{
+    enum
+    {
+        value = WBinom< n - 1, k - 1 >::value + WBinom< n - 1, k >::value
+    };
+};
+
+/**
+ * Specialization for n = k.
+ */
+template< std::size_t n >
+struct WBinom< n, n >
+{
+    enum
+    {
+        value = 1
+    };
+};
+
+/**
+ * Specialization for k = 0.
+ */
+template< std::size_t n >
+struct WBinom< n, 0 >
+{
+    enum
+    {
+        value = 1
+    };
+};
+
+/**
+ * This specialization of the WBinom struct is needed to avoid
+ * infinite recursion in case of k > n. The compiler should abort
+ * compilation with an error message.
+ */
+template< std::size_t k >
+struct WBinom< 0, k >
+{
+};
+
+} // namespace wmath
+
+#endif  // WCOMPILETIMEFUNCTIONS_H
