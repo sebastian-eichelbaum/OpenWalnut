53f05eea162a9672653cc0150cc30bfe1c912e6c
Stefan Philips <philips@informatik.uni-leipzig.de>
2010-05-12 19:59:07 +0200

    Removed LINT-errors

--- src/dataHandler/WValueSet.h
+++ src/dataHandler/WValueSet.h
@@ -156,7 +156,7 @@ private:
 template< typename T > wmath::WVector3D WValueSet< T >::getVector3D( size_t index ) const
 {
     WAssert( m_order == 1 && m_dimension == 3, "WValueSet<T>::getVector3D only implemented for order==1, dim==3 value sets" );
-    WAssert( (index + 1) * 3 <= m_data.size(), "index in WValueSet<T>::getVector3D too big" );
+    WAssert( ( index + 1 ) * 3 <= m_data.size(), "index in WValueSet<T>::getVector3D too big" );
     size_t offset = index * 3;
     return wmath::WVector3D( m_data[ offset ], m_data[ offset + 1 ], m_data[ offset + 2 ] );
 }
@@ -164,7 +164,7 @@ template< typename T > wmath::WVector3D WValueSet< T >::getVector3D( size_t inde
 template< typename T > wmath::WValue< T > WValueSet< T >::getWValue( size_t index ) const
 {
     WAssert( m_order == 1, "WValueSet<T>::getWValue only implemented for order==1 value sets" );
-    WAssert( (index + 1) * m_dimension <= m_data.size(), "index in WValueSet<T>::getWValue too big" );
+    WAssert( ( index + 1 ) * m_dimension <= m_data.size(), "index in WValueSet<T>::getWValue too big" );
 
     size_t offset = index * m_dimension;
 
