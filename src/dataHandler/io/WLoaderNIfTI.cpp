6eac93c526d98a0e710b9036345affdc1f625ca8
Alexander Wiebel <wiebel_openwalnut@dergrosse.de>
2010-05-28 16:09:41 +0200

    [FIX #324] check for reading error in NIfTI loader. This prevents OW from crashing.

--- src/dataHandler/io/WLoaderNIfTI.cpp
+++ src/dataHandler/io/WLoaderNIfTI.cpp
@@ -84,6 +84,8 @@ boost::shared_ptr< WDataSet > WLoaderNIfTI::load()
 {
     nifti_image* header = nifti_image_read( m_fileName.c_str(), 0 );
 
+    WAssert( header, "Error durring file access to NIfTI file. This probably means that the file is corrupted." );
+
     WAssert( header->ndim >= 3,
              "The NIfTI file contains data that has less than the three spatial dimension. OpenWalnut is not able to handle this." );
     int columns = header->dim[1];
