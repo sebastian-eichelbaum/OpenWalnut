9650cf61b851cd4ea3c13f11b257acc3a3b5f894
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2010-07-28 20:00:47 +0200

    [FIX] - minor thread-safety issue

--- src/modules/superquadricGlyphs/WMSuperquadricGlyphs.cpp
+++ src/modules/superquadricGlyphs/WMSuperquadricGlyphs.cpp
@@ -402,9 +402,9 @@ void WMSuperquadricGlyphs::moduleMain()
     ready();
 
     // create all these geodes we need
-    WKernel::getRunningKernel()->getGraphicsEngine()->getScene()->insert( m_output );
     osg::ref_ptr< osg::StateSet > sset = m_output->getOrCreateStateSet();
     sset->setMode( GL_LIGHTING, osg::StateAttribute::OFF );
+    WKernel::getRunningKernel()->getGraphicsEngine()->getScene()->insert( m_output );
 
     // add shader
     m_shader->apply( m_output );
