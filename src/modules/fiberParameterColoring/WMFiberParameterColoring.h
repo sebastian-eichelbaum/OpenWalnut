f2ed7cbaba6c8aa8d9f9451147662471fffca7fa
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2010-08-12 21:52:59 +0200

    [ADD] - skeleton for fiber coloring algorithm alowing fibers to be colored using, for example, angles between segments ...

---
+++ src/modules/fiberParameterColoring/WMFiberParameterColoring.h
@@ -0,0 +1,116 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV@Uni-Leipzig and CNCF@MPI-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WMFIBERPARAMETERCOLORING_H
+#define WMFIBERPARAMETERCOLORING_H
+
+#include <string>
+
+#include "../../dataHandler/WDataSetScalar.h"
+
+#include "../../kernel/WModule.h"
+#include "../../kernel/WModuleInputData.h"
+#include "../../kernel/WModuleOutputData.h"
+
+/**
+ * This modules takes a dataset and equalizes its histogram.
+ *
+ * \ingroup modules
+ */
+class WMFiberParameterColoring: public WModule
+{
+public:
+
+    /**
+     * Default constructor.
+     */
+    WMFiberParameterColoring();
+
+    /**
+     * Destructor.
+     */
+    virtual ~WMFiberParameterColoring();
+
+    /**
+     * Gives back the name of this module.
+     * \return the module's name.
+     */
+    virtual const std::string getName() const;
+
+    /**
+     * Gives back a description of this module.
+     * \return description to module.
+     */
+    virtual const std::string getDescription() const;
+
+    /**
+     * Due to the prototype design pattern used to build modules, this method returns a new instance of this method. NOTE: it
+     * should never be initialized or modified in some other way. A simple new instance is required.
+     *
+     * \return the prototype used to create every module in OpenWalnut.
+     */
+    virtual boost::shared_ptr< WModule > factory() const;
+
+    /**
+     * Get the icon for this module in XPM format.
+     */
+    virtual const char** getXPMIcon() const;
+
+protected:
+
+    /**
+     * Entry point after loading the module. Runs in separate thread.
+     */
+    virtual void moduleMain();
+
+    /**
+     * Initialize the connectors this module is using.
+     */
+    virtual void connectors();
+
+    /**
+     * Initialize the properties for this module.
+     */
+    virtual void properties();
+
+private:
+
+    /**
+     * The fiber dataset which is going to be filtered.
+     */
+    boost::shared_ptr< WModuleInputData< WDataSetFibers > > m_fiberInput;
+
+    /**
+     * The output connector used to provide the calculated data to other modules.
+     */
+    boost::shared_ptr< WModuleOutputData< WDataSetFibers > > m_fiberOutput;
+
+    /**
+     * A condition used to notify about changes in several properties.
+     */
+    boost::shared_ptr< WCondition > m_propCondition;
+};
+
+#endif  // WMFIBERPARAMETERCOLORING_H
+
