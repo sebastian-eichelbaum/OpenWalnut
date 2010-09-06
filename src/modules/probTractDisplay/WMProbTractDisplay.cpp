1e530d6186804b967caa912e98b6b8b6b6b79d42
math <math@informatik.uni-leipzig.de>
2010-09-06 22:20:13 +0200

    [ADD] Now three isosurfaces are possible.

--- src/modules/probTractDisplay/WMProbTractDisplay.cpp
+++ src/modules/probTractDisplay/WMProbTractDisplay.cpp
@@ -23,22 +23,11 @@
 //---------------------------------------------------------------------------
 
 #include <string>
-#include <utility>
 #include <vector>
 
-#include <osg/ShapeDrawable>
-#include <osg/Group>
-#include <osg/Geode>
-#include <osg/Material>
-#include <osg/StateAttribute>
+#include <boost/lexical_cast.hpp>
 
-#include "../../common/WColor.h"
 #include "../../dataHandler/WDataSetScalar.h"
-#include "../../dataHandler/WDataTexture3D.h"
-#include "../../graphicsEngine/WGEGeodeUtils.h"
-#include "../../graphicsEngine/WGEUtils.h"
-#include "../../graphicsEngine/WShader.h"
-#include "../../kernel/WKernel.h"
 #include "probtractdisplay.xpm"
 #include "WMProbTractDisplay.h"
 
@@ -46,8 +35,7 @@
 W_LOADABLE_MODULE( WMProbTractDisplay )
 
 WMProbTractDisplay::WMProbTractDisplay():
-    WModuleContainer(),
-    m_rootNode( new osg::Node() )
+    WModuleContainer()
 {
 }
 
@@ -77,8 +65,8 @@ const std::string WMProbTractDisplay::getDescription() const
 
 void WMProbTractDisplay::connectors()
 {
-    m_input = boost::shared_ptr< WModuleInputData < WDataSetScalar  > >(
-        new WModuleInputData< WDataSetScalar >( shared_from_this(), "probTract", "The probabilistic tractogram as scalar dataset." )
+    m_input = boost::shared_ptr< WModuleInputForwardData < WDataSetScalar  > >(
+        new WModuleInputForwardData< WDataSetScalar >( shared_from_this(), "probTract", "The probabilistic tractogram as scalar dataset." )
     );
 
     addConnector( m_input );
@@ -86,23 +74,6 @@ void WMProbTractDisplay::connectors()
 }
 
 void WMProbTractDisplay::properties()
-{
-    m_numSurfaces    = m_properties->addProperty( "#Surfaces", "The number of how many surfaces should be used for display", 2 );
-    m_numSurfaces->setMin( 1 );
-    updateSubmoduleInstances();
-}
-
-void WMProbTractDisplay::updateSubmoduleInstances()
-{
-    for( int i = 0; i < m_numSurfaces->get(); ++i )
-    {
-        m_isoSurfaces.push_back( WModuleFactory::getModuleFactory()->create( WModuleFactory::getModuleFactory()->getPrototypeByName( "Isosurface" ) ) ); //NOLINT
-        add( m_isoSurfaces.back() );
-        m_isoSurfaces.back()->isReady().wait();
-    }
-}
-
-void WMProbTractDisplay::updateProperties()
 {
 }
 
@@ -122,117 +93,41 @@ void WMProbTractDisplay::moduleMain()
         {
             break;
         }
-
-        // has the data changed?
-        boost::shared_ptr< WDataSetScalar > newDataSet = m_input->getData();
-        bool dataChanged = ( m_dataSet != newDataSet );
-        if ( dataChanged || !m_dataSet )
-        // this condition will become true whenever the new data is different from the current one or our actual data is NULL. This handles all
-        // cases.
-        {
-            // The data is different. Copy it to our internal data variable:
-            debugLog() << "Received Data.";
-            m_dataSet = newDataSet;
-
-            // invalid data
-            if ( !m_dataSet )
-            {
-                debugLog() << "Invalid Data. Disabling.";
-                continue;
-            }
-        }
     }
-
-    WKernel::getRunningKernel()->getGraphicsEngine()->getScene()->remove( m_rootNode );
 }
 
 void WMProbTractDisplay::initSubModules()
 {
-//   m_isoSurface = WModuleFactory::getModuleFactory()->create( WModuleFactory::getModuleFactory()->getPrototypeByName( "Isosurface" ) );
-//   add( m_isoSurface );
-//   m_isoSurface->isReady().wait();
+    static WColor colorInit[] = { WColor::green, WColor::blue, WColor::red }; // NOLINT
+    std::vector< WColor > preDefinedColors( colorInit, colorInit + sizeof colorInit / sizeof colorInit[ 0 ] );
 
-//   m_clusterSlicer = WModuleFactory::getModuleFactory()->create( WModuleFactory::getModuleFactory()->getPrototypeByName( "Cluster Slicer" ) );
-//   add( m_clusterSlicer );
-//   m_clusterSlicer->isReady().wait();
+    static int init[] = { 100, 30, 0 }; // NOLINT
+    std::vector< int > preDefinedOpacities( init, init + sizeof init / sizeof init[ 0 ] );
 
-//   m_meshRenderer = WModuleFactory::getModuleFactory()->create( WModuleFactory::getModuleFactory()->getPrototypeByName( "Triangle Mesh Renderer" ) ); // NOLINT
-//   add( m_meshRenderer );
-//   m_meshRenderer->isReady().wait();
+    WAssert( preDefinedOpacities.size() == preDefinedColors.size(), "preDefinedColors.size() many iso surfaces => so many colors are needed!" );
 
-//   // preset properties
-//   debugLog() << "Start step submodule properties";
-//   m_detTractClustering->getProperties()->getProperty( "active" )->toPropBool()->set( false, true );
-//   m_voxelizer->getProperties()->getProperty( "Fiber Tracts" )->toPropBool()->set( false, true );
-//   m_voxelizer->getProperties()->getProperty( "Display Voxels" )->toPropBool()->set( false, true );
-//   m_voxelizer->getProperties()->getProperty( "Bounding Box Enable Feature" )->toPropBool()->set( false, true );
-//   m_voxelizer->getProperties()->getProperty( "Lighting" )->toPropBool()->set( false, true );
-//   m_gaussFiltering->getProperties()->getProperty( "Iterations" )->toPropInt()->set( 3, true );
-//   m_clusterSlicer->getProperties()->getProperty( "Show|Hide ISO Voxels" )->toPropBool()->set( false );
-//   m_clusterSlicer->getProperties()->getProperty( "Biggest Component Only" )->toPropBool()->set( false );
-//   m_isoSurface->getProperties()->getProperty( "active" )->toPropBool()->set( false, true );
-//   m_isoSurface->getProperties()->getProperty( "Iso Value" )->toPropDouble()->set( 0.2, true );
-//   m_clusterSlicer->getProperties()->getProperty( "Iso Value" )->toPropDouble()->set(  0.2, true );
-//   debugLog() << "Submodule properties set";
-
-//   // wiring
-//   debugLog() << "Start wiring";
-//   m_paramDS->forward( m_clusterSlicer->getInputConnector( "paramDS" ) );
-
-//   m_gaussFiltering->getInputConnector( "in" )->connect( m_voxelizer->getOutputConnector( "voxelOutput" ) );
-//   m_isoSurface->getInputConnector( "values" )->connect( m_gaussFiltering->getOutputConnector( "out" ) );
-//   m_clusterSlicer->getInputConnector( "cluster" )->connect( m_detTractClustering->getOutputConnector( "clusterOutput" ) );
-//   m_clusterSlicer->getInputConnector( "clusterDS" )->connect( m_gaussFiltering->getOutputConnector( "out" ) );
-//   m_clusterSlicer->getInputConnector( "meshIN" )->connect( m_isoSurface->getOutputConnector( "surface mesh" ) );
-//   m_meshRenderer->getInputConnector( "mesh" )->connect( m_clusterSlicer->getOutputConnector( "meshOUT" ) );
-//   m_meshRenderer->getInputConnector( "colorMap" )->connect( m_clusterSlicer->getOutputConnector( "colorMap" ) );
-
-//   m_voxelizer->getInputConnector( "voxelInput" )->connect( m_detTractClustering->getOutputConnector( "clusterOutput" ) );
-//   m_fibers->forward( m_detTractClustering->getInputConnector( "tractInput" ) ); // init rippling
-//   debugLog() << "Wiring done";
-
-//   // forward properties
-//   m_properties->addProperty( m_detTractClustering->getProperties()->getProperty( "Output cluster ID" ) );
-//   m_properties->addProperty( m_detTractClustering->getProperties()->getProperty( "Max cluster distance" ) );
-//   m_properties->addProperty( m_detTractClustering->getProperties()->getProperty( "Min point distance" ) );
-//   m_properties->addProperty( m_voxelizer->getProperties()->getProperty( "Fiber Tracts" ) );
-//   m_properties->addProperty( m_voxelizer->getProperties()->getProperty( "Center Line" ) );
-//   m_properties->addProperty( m_voxelizer->getProperties()->getProperty( "Lighting" ) );
-//   m_properties->addProperty( m_voxelizer->getProperties()->getProperty( "Fiber Transparency" ) );
-//   m_properties->addProperty( m_gaussFiltering->getProperties()->getProperty( "Iterations" ) );
-//   m_properties->addProperty( m_meshRenderer->getProperties()->getProperty( "Opacity %" ) );
-//   m_properties->addProperty( m_meshRenderer->getProperties()->getProperty( "Mesh Color" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "Show|Hide ISO Voxels" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "Mean Type" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "Show|Hide Slices" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "Planes #X-SamplePoints" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "Planes #Y-SamplePoints" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "Planes Step Width" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "#Planes" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "Biggest Component Only" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "Custom Scale" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "MinScale" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "MaxScale" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "MinScaleColor" ) );
-//   m_properties->addProperty( m_clusterSlicer->getProperties()->getProperty( "MaxScaleColor" ) );
-//   m_properties->addProperty( m_voxelizer->getProperties()->getProperty( "Voxels per Unit" ) );
-//   m_properties->addProperty( m_detTractClustering->getProperties()->getProperty( "Start clustering" ) );
-}
-
-void WMProbTractDisplay::activate()
-{
-    if ( m_rootNode )
+    // The reason for only having three iso surfaces is just simplicity, you may change it as you need, or even make it dynamically if you want
+    for( size_t i = 0; i < preDefinedOpacities.size(); ++i )
     {
-        if ( m_active->get() )
-        {
-            m_rootNode->setNodeMask( 0xFFFFFFFF );
-        }
-        else
-        {
-            m_rootNode->setNodeMask( 0x0 );
-        }
+        m_isoSurfaces.push_back( WModuleFactory::getModuleFactory()->create( WModuleFactory::getModuleFactory()->getPrototypeByName( "Isosurface" ) ) ); // NOLINT
+        add( m_isoSurfaces.back() );
+        m_isoSurfaces.back()->isReady().wait();
+
+        // forward data
+        m_input->forward( m_isoSurfaces.back()->getInputConnector( "values" ) );
+
+        // forward properties of interest, the reason for grouping is: property container cannot hold not-unique names
+        std::string isoSurfaceNum = boost::lexical_cast< std::string >( i );
+        WPropGroup m_group = m_properties->addPropertyGroup( "Isosurface " + isoSurfaceNum,  "Property group for isosurface " + isoSurfaceNum );
+        WPropDouble p0 = m_isoSurfaces.back()->getProperties()->getProperty( "Iso value" )->toPropDouble();
+        p0->setMax( 1000.0 );
+        p0->set( 1000.0 );
+        m_group->addProperty( p0 );
+        WPropInt p1 = m_isoSurfaces.back()->getProperties()->getProperty( "Opacity %" )->toPropInt();
+        p1->set( preDefinedOpacities[i] );
+        m_group->addProperty( p1 );
+        WPropColor p2 = m_isoSurfaces.back()->getProperties()->getProperty( "Surface color" )->toPropColor();
+        p2->set( preDefinedColors[i] );
+        m_group->addProperty( p2 );
     }
-
-    WModule::activate();
 }
-
--- src/modules/probTractDisplay/WMProbTractDisplay.h
+++ src/modules/probTractDisplay/WMProbTractDisplay.h
@@ -28,17 +28,15 @@
 #include <string>
 #include <vector>
 
-#include <osg/Node>
-#include <osg/Uniform>
-
-#include "../../graphicsEngine/WShader.h"
+#include "../../kernel/WKernel.h"
 #include "../../kernel/WModuleContainer.h"
 #include "../../kernel/WModuleInputData.h"
-#include "../../kernel/WModuleOutputData.h"
+#include "../../kernel/WModuleInputForwardData.h"
 
 /**
- * This module build the base for DirectVolumeRendering in OpenWalnut. It uses shader based raytracing and will be able to have multi dimensional
- * transfer functions.
+ * This module generates a visualization of probabilistic tracts using iso surfaces.
+ * Therefor three iso surfaces are generated and the user may choose isovalue, color and
+ * alpha value for each surface.
  * \ingroup modules
  */
 class WMProbTractDisplay: public WModuleContainer
@@ -98,61 +96,16 @@ protected:
     virtual void properties();
 
     /**
-     * The root node used for this modules graphics. For OSG nodes, always use osg::ref_ptr to ensure proper resource management.
-     */
-    osg::ref_ptr< osg::Node > m_rootNode;
-
-    /**
-     * Callback for m_active. Overwrite this in your modules to handle m_active changes separately.
-     */
-    virtual void activate();
-
-    /**
      * In order to use sub modules we need to create, initialize (properties) and wire them.
      */
     void initSubModules();
-
-    /**
-     * Update the submodules incase there has been additionally or less modules selected for isosurface generation.
-     */
-    void updateSubmoduleInstances();
-
-    /**
-     * Update the properties for each isosurface incase the number of isosurfaces has changed.
-     */
-    void updateProperties();
 
 private:
     /**
      * Scalar dataset representing the probability field either in real numbers in [0,1] or gray values or just simply the connectivity
      * score (\#visits per voxel).
      */
-    boost::shared_ptr< WModuleInputData< WDataSetScalar > > m_input;
-
-    /**
-     * This is a pointer to the dataset the module is currently working on.
-     */
-    boost::shared_ptr< WDataSetScalar > m_dataSet;
-
-    /**
-     * The number of ISO-Surfaces.
-     */
-    WPropInt m_numSurfaces;
-
-    /**
-     * For each surface its corresponding isovalue.
-     */
-    std::vector< WPropDouble > m_isoValues;
-
-    /**
-     * The color for each isosurface.
-     */
-    std::vector< WPropColor > m_colors;
-
-    /**
-     * The alpha values for each isosurface.
-     */
-    std::vector< WPropDouble > m_alphaValues;
+    boost::shared_ptr< WModuleInputForwardData< WDataSetScalar > > m_input;
 
     /**
      * Submodules for the iso surface generation.
