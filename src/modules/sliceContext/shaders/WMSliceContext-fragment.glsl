ce748f03071d0577c2da79a82f0bc37f041269bd
Alexander Wiebel <wiebel_openwalnut@dergrosse.de>
2010-09-24 16:45:31 +0200

    [FIX] restored color of lines

--- src/modules/sliceContext/shaders/WMSliceContext-fragment.glsl
+++ src/modules/sliceContext/shaders/WMSliceContext-fragment.glsl
@@ -7,9 +7,9 @@ void main()
 {
     vec4 color = myColor;
     float alpha = 1 - ( gl_TexCoord[0].x / 100. );
-    float darkness = clamp( alpha *.1, 0.0, 1.0);
+    float darkness = clamp( alpha *.3, 0.0, 1.0);
     alpha = clamp( pow( alpha, 100. ) +.1 , 0.0, 1.0);
-    color = vec4( darkness, darkness, 1.0, alpha );
+    color = vec4( darkness, clamp( .5 - darkness, 0.0, 1.0), 1.0, alpha );
 
     gl_FragColor = color;
 }
