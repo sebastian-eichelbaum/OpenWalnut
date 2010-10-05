c6655068e8f4b2cf370910395d9d396591992e2d
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2010-10-05 10:10:12 +0200

    [CHANGE] - it is now possible to have one shared lib providing multiple modules. No changes for existing modules needed.

--- src/kernel/WModule.h
+++ src/kernel/WModule.h
@@ -597,17 +597,28 @@ private:
 };
 
 /**
+ * Simply a list of modules. The type is used by the following macros and typedefs
+ */
+typedef std::vector< boost::shared_ptr< WModule > > WModuleList;
+
+/**
+ * The signature used for the module loading entry point
+ */
+typedef void ( *W_LOADABLE_MODULE_SIGNATURE )( WModuleList& );
+
+/**
  * The following macro is used by modules so the factory can acquire a prototype instance from a shared library using the symbol.
+ * You can write this symbol for your own if you need to add multiple modules to the list. This one is for convenience.
  *
  * \note we need the module instance to be created using a shared_ptr as WModule is derived from enable_shared_from_this. Removing the shared
  *       pointer causes segmentation faults during load.
  */
 #ifdef _MSC_VER
 #define W_LOADABLE_MODULE( MODULECLASS ) \
-extern "C" __declspec(dllexport) void WLoadModule( boost::shared_ptr< WModule > &m ) { m = boost::shared_ptr< WModule >( new MODULECLASS ); }  // NOLINT
+extern "C" __declspec(dllexport) void WLoadModule( WModuleList& m ) { m.push_back( boost::shared_ptr< WModule >( new MODULECLASS ) ); }  // NOLINT
 #else
 #define W_LOADABLE_MODULE( MODULECLASS ) \
-extern "C"                       void WLoadModule( boost::shared_ptr< WModule > &m ) { m = boost::shared_ptr< WModule >( new MODULECLASS ); }  // NOLINT
+extern "C"                       void WLoadModule( WModuleList& m ) { m.push_back( boost::shared_ptr< WModule >( new MODULECLASS ) ); }  // NOLINT
 #endif
 
 /**
