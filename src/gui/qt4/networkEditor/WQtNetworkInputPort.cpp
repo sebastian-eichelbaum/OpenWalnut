36a109b017534d2ea2f1e415fd98bd4015abbffd
skiunke <devnull@localhost>
2010-10-06 17:14:05 +0200

    [ADD] forgotten to add the new files

---
+++ src/gui/qt4/networkEditor/WQtNetworkInputPort.cpp
@@ -0,0 +1,254 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV@Uni-Leipzig and CNCF@MPI-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#include <string>
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+
+#include <QtGui/QGraphicsSceneMouseEvent>
+#include <QtGui/QGraphicsView>
+#include <QtGui/QGraphicsScene>
+#include <QtGui/QGraphicsRectItem>
+#include <QtGui/QGraphicsLineItem>
+#include "WQtNetworkOutputPort.h"
+
+#include "WQtNetworkInputPort.h"
+
+const float SIZE = 10.0;
+
+WQtNetworkInputPort::WQtNetworkInputPort( boost::shared_ptr<WModuleInputConnector> connector )
+    : WQtNetworkPort()
+{
+    setRect( 0.0, 0.0, SIZE, SIZE );
+    setBrush( Qt::gray );
+    setPen( QPen( Qt::red, 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin ) );
+
+    setAcceptsHoverEvents( true );
+
+    setPortName( connector.get()->getName().c_str() );
+    setOutPort( connector.get()->isOutputConnector() );
+    m_connector = connector;
+
+    QString tmp;
+    if( isOutPort() == true ) tmp = "output";
+    else if( isOutPort() == false ) tmp = "input";
+    else
+        tmp = "undefined";
+
+    //TODO
+    QString str = "Name: " + getPortName() + "\nPortType: " + tmp;
+    if( toolTip() != str )
+    {
+        setToolTip( str );
+    }
+}
+
+WQtNetworkInputPort::~WQtNetworkInputPort()
+{
+    removeArrows();
+}
+
+int WQtNetworkInputPort::type() const
+{
+    return Type;
+}
+
+void WQtNetworkInputPort::addArrow( WQtNetworkArrow *arrow )
+{
+    m_arrows.append( arrow );
+}
+
+void WQtNetworkInputPort::removeArrow( WQtNetworkArrow *arrow )
+{
+    int index = m_arrows.indexOf( arrow );
+
+    if (index != -1)
+        m_arrows.removeAt( index );
+}
+
+QString WQtNetworkInputPort::getPortName()
+{
+    return m_name;
+}
+
+void WQtNetworkInputPort::setPortName( QString str )
+{
+    m_name = str;
+}
+
+int WQtNetworkInputPort::getNumberOfArrows()
+{
+    return m_arrows.size();
+}
+
+boost::shared_ptr<WModuleInputConnector> WQtNetworkInputPort::getConnector()
+{
+    return m_connector;
+}
+
+
+//private
+void WQtNetworkInputPort::removeArrows()
+{
+    foreach( WQtNetworkArrow *arrow, m_arrows )
+    {
+        int index = m_arrows.indexOf( arrow );
+        if ( index != -1 )
+        {
+            m_arrows.removeAt( index );
+            delete arrow;
+        }
+    }
+}
+
+void WQtNetworkInputPort::updateArrows()
+{
+    foreach( WQtNetworkArrow *arrow, m_arrows )
+    {
+        arrow->updatePosition();
+    }
+}
+
+void WQtNetworkInputPort::setOutPort( bool type )
+{
+    m_isOutPort = type;
+}
+
+bool WQtNetworkInputPort::isOutPort()
+{
+    return m_isOutPort;
+}
+/*
+void WQtNetworkInputPort::mousePressEvent( QGraphicsSceneMouseEvent *mouseEvent )
+{
+        line = new QGraphicsLineItem( QLineF( mouseEvent->scenePos(), mouseEvent->scenePos() ) );
+        line->setPen( QPen( Qt::black, 2 ) );
+        scene()->addItem( line );
+}
+
+void WQtNetworkInputPort::mouseMoveEvent( QGraphicsSceneMouseEvent *mouseEvent )
+{
+    if( line != 0 )
+    {
+        QLineF newLine( line->line().p1(), mouseEvent->scenePos() );
+
+        QList<QGraphicsItem *> endItem = scene()->items( mouseEvent->scenePos() );
+        // because line is first item below the curser
+        if( !endItem.isEmpty() &&
+                endItem.first()->type() == QGraphicsLineItem::Type )
+        {
+            endItem.removeFirst();
+        }
+
+        if( !endItem.isEmpty() )
+        {
+            if( endItem.first()->type() == WQtNetworkInputPort::Type )
+            {
+                //TODO!!!
+                WQtNetworkInputPort *endPort = qgraphicsitem_cast<WQtNetworkInputPort *>( endItem.first() );
+
+                if( //endPort->isOutPort() == false &&
+                    endPort->parentItem() != this->parentItem() &&
+                    //endPort->getPortName() == this->getPortName() &&
+                    endPort->getNumberOfArrows() < 1
+                    )
+                {
+                   line->setPen( QPen( Qt::green, 2 ) );
+                }
+                else
+                {
+                   line->setPen( QPen( Qt::red, 2 ) );
+                }
+            }
+            else if( endItem.first()->type() == WQtNetworkOutputPort::Type )
+            {   
+                   line->setPen( QPen( Qt::red, 2 ) );
+            }
+            else
+            {
+                line->setPen( QPen( Qt::black, 2 ) );
+            }
+        }
+        else
+        {
+            line->setPen( QPen( Qt::black, 2 ) );
+        }
+        line->setLine( newLine );
+    }
+}
+
+void WQtNetworkInputPort::mouseReleaseEvent( QGraphicsSceneMouseEvent *mouseEvent )
+{
+    if( line != 0 )
+    {
+        QList<QGraphicsItem *> startItems = scene()->items( line->line().p1() );
+        QList<QGraphicsItem *> endItems = scene()->items( line->line().p2() );
+
+        // because line is first item below the curser
+        if( startItems.first()->type() == QGraphicsLineItem::Type )
+        {
+            startItems.removeFirst();
+        }
+
+        // because line is first item below the curser
+        if( endItems.first()->type() == QGraphicsLineItem::Type )
+        {
+            endItems.removeFirst();
+        }
+
+        // remove current line for real connection
+        scene()->removeItem( line );
+        delete line;
+
+        if( !endItems.isEmpty() &&
+             !startItems.isEmpty() &&
+             endItems.first()->type() == WQtNetworkInputPort::Type &&
+             startItems.first()->parentItem() != endItems.first()->parentItem() )
+        {
+            WQtNetworkOutputPort *startPort = qgraphicsitem_cast<WQtNetworkOutputPort *>( startItems.first() );
+            WQtNetworkInputPort *endPort = qgraphicsitem_cast<WQtNetworkInputPort *>( endItems.first() );
+
+            std::cout << "PORTS CONNECTBAR: " << endPort->getConnector()->connectable( startPort->getConnector() ) << std::endl;
+
+            //TODO skiunke: isOutput realy needed?
+            if( endPort->getNumberOfArrows() < 1 &&
+                    endPort->isOutPort() == false &&
+                    startPort->isOutPort() == true &&
+                    //endPort->getPortName() == startPort->getPortName() )
+                    endPort->getConnector()->connectable( startPort->getConnector() ) == true )
+            {
+                WQtNetworkArrow *arrow = new WQtNetworkArrow( startPort, endPort );
+
+                arrow->setZValue( -1000.0 );
+
+                startPort->addArrow( arrow );
+                endPort->addArrow( arrow );
+
+                scene()->addItem( arrow );
+                arrow->updatePosition();
+            }
+        }
+    }
+}
+*/
---
+++ src/gui/qt4/networkEditor/WQtNetworkOutputPort.h
@@ -0,0 +1,166 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV@Uni-Leipzig and CNCF@MPI-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WQTNETWORKOUTPUTPORT_H
+#define WQTNETWORKOUTPUTPORT_H
+
+#include <QtGui/QGraphicsRectItem>
+#include <QtGui/QGraphicsLineItem>
+#include <QtGui/QGraphicsScene>
+
+#include "../../../kernel/WModuleOutputConnector.h"
+#include "WQtNetworkPort.h"
+#include "WQtNetworkArrow.h"
+
+// forward declaration
+class WQtNetworkArrow;
+//class WQtNetworkPort;
+
+/**
+ * This class represents the ports a module have. Two ports from different
+ * modules can be connected by the corresponding ports.
+ */
+class WQtNetworkOutputPort : public WQtNetworkPort
+{
+    public:
+
+        /**
+         * Constructor
+         */
+        explicit WQtNetworkOutputPort( boost::shared_ptr<WModuleOutputConnector> connector );
+
+        /**
+         * Destructor
+         */
+        ~WQtNetworkOutputPort();
+
+        /**
+         * This customize the return value of type()
+         */
+        enum
+        {
+            Type = UserType + 11
+        };
+
+        /**
+         * Reimplementation from QGraphicsItem
+         * \return the type of the item as int
+         */
+        int type() const;
+
+        /**
+         * Every Arrow connected with this port is updating its position in the
+         * scene.
+         */
+        void updateArrows();
+
+        /**
+         * Removes a specific arrow
+         * \param arrow an arrow
+         */
+        void removeArrow( WQtNetworkArrow *arrow );
+
+        /**
+         * Removes all connected arrows
+         */
+        void removeArrows();
+
+        /**
+         * Calculates the position inside a item for each port to get a correct
+         * alignment
+         * \param size the total number of ports ( distinguished by in- and
+         *          outport
+         * \param portNumber the number of the current port (distinguised by in-
+         *          and outport
+         * \param rect the rect of the parent item
+         * \param inOut is it an in- or outport
+         */
+        void alignPosition( int size, int portNumber, QRectF rect, bool outPort );
+
+        /**
+         * Set the type of the port.
+         * \param type true if out / false if in
+         */
+        void setOutPort( bool type );
+
+        /**
+         * Returns the porttype.
+         * \return is it a outport
+         */
+        bool isOutPort();
+
+        /**
+         * Returns the Name.
+         * \return Name
+         */
+        QString getPortName();
+
+        /**
+         * Set the Name
+         * \param str Name as string
+         */
+        void setPortName( QString str );
+
+        int getNumberOfArrows();
+
+        boost::shared_ptr<WModuleOutputConnector> getConnector();
+
+        /**
+         * Adds an arrow to the port
+         */
+        void addArrow( WQtNetworkArrow *arrow );
+
+    protected:
+
+        /**
+         * Start drawing an arrow
+         *
+         * \param mouseEvent the mouse event
+         */
+//        void mousePressEvent( QGraphicsSceneMouseEvent *mouseEvent );
+
+        /**
+         * Updates the arrows endpoint.
+         *
+         * \param mouseEvent the mouse event
+         */
+//        void mouseMoveEvent( QGraphicsSceneMouseEvent *mouseEvent );
+
+        /**
+         * Draw the arrow if its the correct port
+         *
+         * \param mouseEvent the mouse event
+         */
+//        void mouseReleaseEvent( QGraphicsSceneMouseEvent *mouseEvent );
+
+
+    private:
+
+        QGraphicsLineItem *line; //!< the temporary line when u connect two ports
+        bool m_isOutPort; //!< is the port an outport
+        QList< WQtNetworkArrow *> m_arrows; //!< the connected arrows
+        QString m_name; //!< the portname
+        boost::shared_ptr<WModuleOutputConnector> m_connector;
+};
+#endif  // WQTNETWORKOUTPUTPORT_H
