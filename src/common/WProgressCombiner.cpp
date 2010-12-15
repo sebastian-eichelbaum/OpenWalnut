c1c74631837f88d86ace981c293ee852d310fa0b
math <math@informatik.uni-leipzig.de>
2010-12-15 13:12:55 +0100

    [FIX] Now the modules tool-tip shows correctly if the module is waiting or busy, or a problem has occurred.

--- src/common/WProgressCombiner.cpp
+++ src/common/WProgressCombiner.cpp
@@ -22,6 +22,7 @@
 //
 //---------------------------------------------------------------------------
 
+#include <sstream>
 #include <string>
 #include <set>
 
@@ -76,6 +77,25 @@ void WProgressCombiner::update()
     }
 
     rlock.unlock();
+}
+
+std::string WProgressCombiner::getCombinedNames() const
+{
+    std::stringstream ss;
+    ss << "[";
+    for ( std::set< boost::shared_ptr< WProgress > >::iterator i = m_children.begin(); i != m_children.end(); ++i )
+    {
+        // enforce child to update
+        ss << ( *i )->getName();
+        std::set< boost::shared_ptr< WProgress > >::iterator j = i; // for last element determination
+        if( ++j != m_children.end() )
+        {
+            ss << ", ";
+        }
+    }
+    ss << "]";
+
+    return ss.str();
 }
 
 void WProgressCombiner::addSubProgress( boost::shared_ptr< WProgress > progress )
