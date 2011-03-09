4f9a95fe40f3d9075c8ef08b2ef768d585e41468
math <math@informatik.uni-leipzig.de>
2011-03-09 11:35:59 +0100

    [CHANGE] Refactored shader code, which now should do the same but with less code.

--- src/modules/boundaryCurvesWMGM/shaders/WMBoundaryCurvesWMGM-fragment.glsl
+++ src/modules/boundaryCurvesWMGM/shaders/WMBoundaryCurvesWMGM-fragment.glsl
@@ -22,6 +22,8 @@
 //
 //---------------------------------------------------------------------------
 
+#version 120
+
 uniform sampler3D u_texture0Sampler;
 
 uniform int u_texture0SizeX;
@@ -29,30 +31,12 @@ uniform int u_texture0SizeX;
 uniform float u_grayMatter;
 uniform float u_whiteMatter;
 
-void lookupTex( inout vec4 col, in sampler3D tex, in float threshold, in vec3 v, in float alpha )
-{
-    vec4 col1 = vec4( 0.0 );
-
-    col1 = clamp( texture3D( tex, v ), 0.0, 1.0 );
-    if( col1.a < 1.0 )
-    {
-        alpha = 0.0;
-    }
-
-    col.rgb = col1.rgb;
-}
-
 void main()
 {
-    vec4 col = vec4( 0.0, 0.0, 0.0, 1.0 );
-
-    lookupTex( col, u_texture0Sampler, u_grayMatter, gl_TexCoord[0].xyz, u_whiteMatter );
-
-    col = clamp( col, 0.0, 1.0 );
+    vec4 col = clamp( texture3D( u_texture0Sampler, gl_TexCoord[0].xyz ), 0.0, 1.0 );
 
     vec3 grad;
     {
-        // find a proper normal for a headlight
         float s = 0.01;
         vec3 curPoint = gl_TexCoord[0].xyz;
         float valueXP = texture3D( u_texture0Sampler, curPoint + vec3( s, 0.0, 0.0 ) ).r;
@@ -74,17 +58,13 @@ void main()
     }
     if( abs( col.r - u_grayMatter ) < eps )
     {
-        col.rgb =  vec3( 0, 0, 0 );
-        float l = length( grad ) / 5.0;
-        col.rgb = vec3( l, l, l );
-        col.a = 1;
+        col.rgb = vec3( 0.0 );
     }
-    if( abs( col.r - u_whiteMatter ) < eps )
+    else // if( abs( col.r - u_whiteMatter ) < eps )
     {
-        col.rgb = normalize( grad );
         col.rgb = vec3( 0.5 );
-        col.a = 1;
     }
 
+    col.a = 1;
     gl_FragColor = col;
 }
