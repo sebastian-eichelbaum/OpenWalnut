bea4e18fb0af5a0727b193c611e203aba1fd95de
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2011-04-07 12:06:03 +0200

    [FIX] fixed doc. operator() in Eigen Matrix is row,column.

--- src/common/math/WMatrix.h
+++ src/common/math/WMatrix.h
@@ -41,7 +41,7 @@
  * [0 3 6
  *  1 4 7
  *  2 5 8]
- * If you want to access coefficients using the operator( size_t, size_t ), the first parameter is the column index, starting with 0.
+ * If you want to access coefficients using the operator( size_t, size_t ), the first parameter is still the row index, starting with 0.
  *
  * \see http://eigen.tuxfamily.org/dox/classEigen_1_1Matrix.html
  * \see http://eigen.tuxfamily.org/dox/classEigen_1_1MatrixBase.html
@@ -56,7 +56,7 @@ typedef Eigen::Matrix< double, 3, 3 > WMatrix3x3_2;
  *  1 5 9  13
  *  2 6 10 14
  *  3 7 11 15]
- * If you want to access coefficients using the operator( size_t, size_t ), the first parameter is the column index, starting with 0.
+ * If you want to access coefficients using the operator( size_t, size_t ), the first parameter is still the row index, starting with 0.
  *
  * \see http://eigen.tuxfamily.org/dox/classEigen_1_1Matrix.html
  * \see http://eigen.tuxfamily.org/dox/classEigen_1_1MatrixBase.html
@@ -65,7 +65,7 @@ typedef Eigen::Matrix< double, 4, 4 > WMatrix4x4_2;
 
 /**
  * A double matrix of dynamic size. Heap-allocated. Column Major!
- * If you want to access coefficients using the operator( size_t, size_t ), the first parameter is the column index, starting with 0.
+ * If you want to access coefficients using the operator( size_t, size_t ), the first parameter is still the row index, starting with 0.
  *
  * \see http://eigen.tuxfamily.org/dox/classEigen_1_1Matrix.html
  * \see http://eigen.tuxfamily.org/dox/classEigen_1_1MatrixBase.html
