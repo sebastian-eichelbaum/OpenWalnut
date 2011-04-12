7a9a34f7e046ece8d4e13de37cc7672be5123eca
Alexander Wiebel <wiebel_openwalnut@dergrosse.de>
2011-04-12 19:41:45 +0200

    [FIX] need to tell cmake which build type we want

--- tools/initScript_linux.bash
+++ tools/initScript_linux.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir build/release
 cd build/release
-cmake ../../src
+cmake  -D CMAKE_BUILD_TYPE:STRING=Release ../../src
 make many
 cd ../..
 
 mkdir build/debug
 cd build/debug
-cmake ../../src
+cmake  -D CMAKE_BUILD_TYPE:STRING=Debug ../../src
 make many
 cd ../..
