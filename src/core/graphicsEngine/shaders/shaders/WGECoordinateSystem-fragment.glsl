78c56118bc12cbb505d1d4c7dafb033d24d8a826
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2011-07-14 18:06:49 +0200

    [ADD] - added nice utility to create sophisticated coordinate system representations with the WGE. This work was done by David Geistert.

---
+++ src/core/graphicsEngine/shaders/shaders/WGECoordinateSystem-fragment.glsl
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV-Leipzig and CNCF-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#version 120
+
+#include "WGEShadingTools.glsl"
+
+/**
+ * The normal.
+ */
+varying vec3 v_normal;
+
+void main()
+{
+    vec4 col = gl_FrontMaterial.diffuse;
+    vec4 white = vec4( 1, 1, 1, 1 );
+
+    // calculate lighting
+    float light = blinnPhongIlluminationIntensity( normalize( v_normal ) );
+    col *= light;
+
+    // use texture coordinate to mix along the cylinder
+    gl_FragColor = mix( white, col, gl_TexCoord[0].t + 0.2 );
+}
