6c3d5d289254791b61a5f243a95416e4f3986a68
Alexander Wiebel <wiebel_openwalnut@dergrosse.de>
2011-10-09 17:28:41 +0200

    [ADD #93] functionality to read all data types supported by openwalnut

--- src/modules/readRawData/WMReadRawData.h
+++ src/modules/readRawData/WMReadRawData.h
@@ -26,6 +26,7 @@
 #define WMREADRAWDATA_H
 
 #include <string>
+#include <vector>
 
 #include <osg/Geode>
 
@@ -35,13 +36,7 @@
 #include "core/kernel/WModuleOutputData.h"
 
 /**
- * Someone should add some documentation here.
- * Probably the best person would be the module's
- * creator, i.e. "wiebel".
- *
- * This is only an empty template for a new module. For
- * an example module containing many interesting concepts
- * and extensive documentation have a look at "src/modules/template"
+ * A module to read scalar data stored as array of raw data.
  *
  * \ingroup modules
  */
@@ -50,12 +45,12 @@ class WMReadRawData: public WModule
 public:
 
     /**
-     *
+     * Constructor of module.
      */
     WMReadRawData();
 
     /**
-     *
+     * Destructor of module.
      */
     virtual ~WMReadRawData();
 
@@ -119,14 +114,61 @@ private:
      */
     boost::shared_ptr< WDataSetScalar > readData( std::string fileName );
 
+    /**
+     * Helper function to read data of the given data type.
+     *
+     * \param fileName Location of the data file.
+     *
+     * \return A vector containing the scalar data values.
+     */
+    template< class T >  boost::shared_ptr< std::vector< T > > readDataTyped( std::string fileName );
+
     boost::shared_ptr< WCondition > m_propCondition;  //!< A condition used to notify about changes in several properties.
     WPropFilename m_dataFile; //!< The data will be read from this file.
     WPropInt m_X; //!< Samples in X direction
     WPropInt m_Y; //!< Samples in Y direction
     WPropInt m_Z; //!< Samples in Z direction
 
+    boost::shared_ptr< WItemSelection > m_dataTypeSelectionsList; //!< A list of file type selection types
+    WPropSelection m_dataTypeSelection; //!< Selection property for file types
+
     boost::shared_ptr< WDataSetScalar > m_dataSet; //!< This data set is provided as output through the connector.
     boost::shared_ptr< WModuleOutputData< WDataSetScalar > > m_output;  //!< Output connector provided by this module.
 };
+
+
+
+template< class T >
+boost::shared_ptr< std::vector< T > > WMReadRawData::readDataTyped( std::string fileName )
+{
+    size_t numX = m_X->get();
+    size_t numY = m_Y->get();
+    size_t numZ = m_Z->get();
+    size_t numVoxels = numX * numY * numZ;
+    boost::shared_ptr< std::vector< T > > values;
+    values = boost::shared_ptr< std::vector< T > >( new std::vector< T >( numVoxels ) );
+
+
+    std::ifstream ifs;
+    ifs.open( fileName.c_str(), std::ifstream::in|std::ios::binary );
+    if( !ifs || ifs.bad() )
+    {
+        WLogger::getLogger()->addLogMessage( "Try load broken file '" + fileName + "'", "Read Raw Data", LL_ERROR );
+        throw std::runtime_error( "Problem during reading file. Probably file not found." );
+    }
+
+    T *pointData = new T[ numVoxels ];
+    ifs.read( reinterpret_cast< char* >( pointData ), sizeof( T ) * numVoxels );
+
+    //T *pointDataAsT = reinterpret_cast< T* >( pointData );
+
+    for( size_t voxelId = 0; voxelId < numVoxels; ++voxelId )
+    {
+        (*values)[voxelId] = pointData[voxelId];
+    }
+    ifs.close();
+
+    return values;
+}
 
 #endif  // WMREADRAWDATA_H
