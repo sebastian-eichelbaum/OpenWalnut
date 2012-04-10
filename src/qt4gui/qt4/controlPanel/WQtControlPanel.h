5e1d04e785ec1968ffdc3887a13c06859c7d1723
Alexander Wiebel <wiebel_openwalnut@dergrosse.de>
2012-04-10 22:29:26 +0200

    [STYLE #42] corrected acronym capitalization

--- src/qt4gui/qt4/controlPanel/WQtControlPanel.h
+++ src/qt4gui/qt4/controlPanel/WQtControlPanel.h
@@ -109,16 +109,16 @@ public:
     WQtModuleTreeItem* addModule( boost::shared_ptr< WModule > module );
 
     /**
-     * Adds a roi entry to the control panel
+     * Adds a ROI entry to the control panel
      *
-     * \param roi pointer to the roi representation object
+     * \param roi pointer to the ROI representation object
      */
     void addRoi( osg::ref_ptr< WROI > roi );
 
     /**
-     * Removes a roi entry from the control panel
+     * Removes a ROI entry from the control panel
      *
-     * \param roi pointer to the roi representation object
+     * \param roi pointer to the ROI representation object
      */
     void removeRoi( osg::ref_ptr< WROI > roi );
 
@@ -135,16 +135,16 @@ public:
     boost::shared_ptr< WModule > getSelectedModule();
 
     /**
-     * Returns the currently selected roi.
+     * Returns the currently selected ROI.
      *
-     * \return pointer to roi representation
+     * \return pointer to ROI representation
      */
     osg::ref_ptr< WROI > getSelectedRoi();
 
     /**
-     * Returns the first roi in the currently selected branch.
+     * Returns the first ROI in the currently selected branch.
      *
-     * \return pointer to roi representation
+     * \return pointer to ROI representation
      */
     osg::ref_ptr< WROI > getFirstRoiInSelectedBranch();
 
@@ -284,11 +284,11 @@ private:
 
     WQtModuleHeaderTreeItem* m_tiModules; //!< header for modules
 
-    WQtRoiHeaderTreeItem* m_tiRois; //!< header for rois
+    WQtRoiHeaderTreeItem* m_tiRois; //!< header for ROIs
 
     bool m_showToolBarText; //!< Show tool bar icons with text
 
-    QDockWidget* m_roiDock;     //!< the dock widget with the roi tree
+    QDockWidget* m_roiDock;     //!< the dock widget with the ROI tree
     QDockWidget* m_moduleDock;  //!< the dock widget with the module tree
 
     /**
@@ -436,7 +436,7 @@ private slots:
     void deleteModule();
 
     /**
-     * function to notify the roi manager of any drag&drop action in the roi tree
+     * function to notify the ROI manager of any drag&drop action in the ROI tree
      */
     void handleDragDrop();
 };
