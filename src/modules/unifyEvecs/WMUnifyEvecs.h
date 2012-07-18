b23f54434a9a25c4406ae33abc3ef949b337feaf
math <math@informatik.uni-leipzig.de>
2012-07-18 21:45:25 +0200

    [ADD] New module: unifyEvecs. This realigns vectors, by mirroring them (if neccessary) into the same halfspace.

---
+++ src/modules/unifyEvecs/WMUnifyEvecs.h
@@ -0,0 +1,115 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV-Leipzig and CNCF-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WMUNIFYEVECS_H
+#define WMUNIFYEVECS_H
+
+#include <string>
+
+#include <osg/Geode>
+
+#include "core/kernel/WModule.h"
+#include "core/kernel/WModuleInputData.h"
+#include "core/kernel/WModuleOutputData.h"
+
+/**
+ * This module aligns diffusion directions so that they are inside of the same halfspace.
+ * This is done mainly for trilinear interpolation which would produce artifacts otherwise.
+ * \ingroup modules
+ */
+class WMUnifyEvecs: public WModule
+{
+public:
+    /**
+     *
+     */
+    WMUnifyEvecs();
+
+    /**
+     *
+     */
+    virtual ~WMUnifyEvecs();
+
+    /**
+     * Gives back the name of this module.
+     * \return the module's name.
+     */
+    virtual const std::string getName() const;
+
+    /**
+     * Gives back a description of this module.
+     * \return description to module.
+     */
+    virtual const std::string getDescription() const;
+
+    /**
+     * Due to the prototype design pattern used to build modules, this method returns a new instance of this method. NOTE: it
+     * should never be initialized or modified in some other way. A simple new instance is required.
+     *
+     * \return the prototype used to create every module in OpenWalnut.
+     */
+    virtual boost::shared_ptr< WModule > factory() const;
+
+    /**
+     * Get the icon for this module in XPM format.
+     *
+     * \return The icon.
+     */
+    virtual const char** getXPMIcon() const;
+
+protected:
+    /**
+     * Entry point after loading the module. Runs in separate thread.
+     */
+    virtual void moduleMain();
+
+    /**
+     * Initialize the connectors this module is using.
+     */
+    virtual void connectors();
+
+    /**
+     * Initialize the properties for this module.
+     */
+    virtual void properties();
+
+    /**
+     * Initialize requirements for this module.
+     */
+    virtual void requirements();
+
+
+private:
+    /**
+     * Input connector for the vector dataset.
+     */
+    boost::shared_ptr< WModuleInputData< WDataSetVector > > m_vectorIC;
+
+    /**
+     * Output connector for the vector dataset.
+     */
+    boost::shared_ptr< WModuleOutputData< WDataSetVector > > m_vectorOC;
+};
+
+#endif  // WMUNIFYEVECS_H
