71af2205d6880df9db48d031ccd9b9982f6157a1
Mario Hlawitschka <hlawit@informatik.uni-leipzig.de>
2012-08-11 16:19:41 +0200

    [FIX] use non-transparent alpha values in the colors as the gui cannot handle them

--- src/modules/transferFunction1D/WMTransferFunction1D.cpp
+++ src/modules/transferFunction1D/WMTransferFunction1D.cpp
@@ -87,9 +87,9 @@ void WMTransferFunction1D::properties()
     m_propCondition = boost::shared_ptr< WCondition >( new WCondition() );
     WTransferFunction tf;
     tf.addAlpha( 0.0, 0.0 );
-    tf.addColor( 0.0, WColor( 0.0, 0.0, 0.0, 0.0 ) );
+    tf.addColor( 0.0, WColor( 0.0, 0.0, 0.0, 1.0 ) );
     tf.addAlpha( 1.0, 0.5 );
-    tf.addColor( 1.0, WColor( 1.0, 0.0, 0.0, 0.0 ) );
+    tf.addColor( 1.0, WColor( 1.0, 0.0, 0.0, 1.0 ) );
     m_transferFunction = m_properties->addProperty( "Transfer Function", "The transfer function editor.", tf, m_propCondition, false );
 
     m_binSize = m_properties->addProperty( "Histogram Resolution", "Number of bins in histogram.", 64, m_propCondition );
