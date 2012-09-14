f8eb68a9acd3062bf0867b96501ce5989a6a3422
Alexander Wiebel <wiebel_openwalnut@dergrosse.de>
2012-09-14 14:36:19 +0200

    [ADD #210] very basic first version of module than can load some of the HxSpatial grid data from amiraMesh files

---
+++ src/modules/readAmiraMesh/WMReadAmiraMesh.cpp
@@ -0,0 +1,245 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV-Leipzig and CNCF-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#include <fstream>
+#include <string>
+
+#include "WMReadAmiraMesh.xpm"
+#include "core/common/WIOTools.h"
+#include "core/common/WPathHelper.h"
+#include "core/dataHandler/WDataSetFiberVector.h"
+#include "core/dataHandler/WDataSetFibers.h"
+#include "core/kernel/WKernel.h"
+
+#include "WMReadAmiraMesh.h"
+
+// This line is needed by the module loader to actually find your module. Do not remove. Do NOT add a ";" here.
+W_LOADABLE_MODULE( WMReadAmiraMesh )
+
+WMReadAmiraMesh::WMReadAmiraMesh():
+    WModule()
+{
+}
+
+WMReadAmiraMesh::~WMReadAmiraMesh()
+{
+    // Cleanup!
+}
+
+boost::shared_ptr< WModule > WMReadAmiraMesh::factory() const
+{
+    return boost::shared_ptr< WModule >( new WMReadAmiraMesh() );
+}
+
+const char** WMReadAmiraMesh::getXPMIcon() const
+{
+    return WMReadAmiraMesh_xpm;
+}
+const std::string WMReadAmiraMesh::getName() const
+{
+    return "[IN DEVELOPMENT] Read Amira Mesh";
+}
+
+const std::string WMReadAmiraMesh::getDescription() const
+{
+    return "Read AmiraMesh file format. At the moment only spatial graphs are supported.";
+}
+
+void WMReadAmiraMesh::connectors()
+{
+    // initialize connectors
+    m_output = boost::shared_ptr< WModuleOutputData< WDataSetFibers > >( new WModuleOutputData< WDataSetFibers >(
+                shared_from_this(), "out", "A loaded dataset." )
+            );
+
+    // add it to the list of connectors. Please note, that a connector NOT added via addConnector will not work as expected.
+    addConnector( m_output );
+
+    WModule::connectors();
+}
+
+void WMReadAmiraMesh::properties()
+{
+    m_propCondition = boost::shared_ptr< WCondition >( new WCondition() );
+    m_dataFile = m_properties->addProperty( "Filename", "", WPathHelper::getAppPath(), m_propCondition );
+
+    WModule::properties();
+}
+
+void WMReadAmiraMesh::requirements()
+{
+    // Put the code for your requirements here. See "src/modules/template/" for an extensively documented example.
+}
+
+void WMReadAmiraMesh::moduleMain()
+{
+    m_moduleState.add( m_propCondition );
+    ready();
+    while( !m_shutdownFlag() )
+    {
+        m_moduleState.wait();
+
+        if( m_shutdownFlag() )
+        {
+            break;
+        }
+
+        bool readingSuccessful = readAmiraMesh( m_dataFile->get().string() );
+        if( readingSuccessful )
+        {
+            prepareResult();
+        }
+    }
+}
+
+void WMReadAmiraMesh::prepareResult()
+{
+    boost::shared_ptr< std::vector< WFiber > > fibs( new std::vector< WFiber > );
+    for( size_t edgeId = 0; edgeId < m_edges.size(); ++edgeId )
+    {
+        std::vector< WPosition > points;
+        points.push_back( m_vertices[m_edges[edgeId].first] );
+        points.push_back( m_vertices[m_edges[edgeId].second] );
+
+        fibs->push_back( WFiber( points ) );
+    }
+    WDataSetFiberVector fibersVector( fibs );
+    m_graph = boost::shared_ptr< WDataSetFibers >( fibersVector.toWDataSetFibers() );
+    m_output->updateData( m_graph );
+}
+
+bool checkVersionString( std::string line )
+{
+    return line == "# AmiraMesh 3D ASCII 2.0";
+}
+
+size_t parseDefine( std::string line )
+{
+    std::vector< std::string > tokens = string_utils::tokenize( line );
+    return string_utils::fromString< double >( tokens[2] );
+}
+
+void skipEmptyAndCommentLines( std::ifstream& data, std::string& line )
+{
+    getline( data, line );
+    while( line == std::string("") || line[0] == '#' )
+    {
+        getline( data, line );
+    }
+}
+
+bool parseParameters( std::ifstream& data, std::string& line )
+{
+    bool contenTypeIsHxSpatialGraph = false;
+    while( line != std::string("}") )
+    {
+        if( line.find( "ContentType" ) != std::string::npos
+            && line.find( "HxSpatialGraph" ) != std::string::npos )
+        {
+            contenTypeIsHxSpatialGraph = true;
+        }
+
+        getline( data, line );
+    }
+    return contenTypeIsHxSpatialGraph;
+}
+
+bool WMReadAmiraMesh::readAmiraMesh( std::string fileName )
+{
+    std::ifstream dataFile( fileName.c_str() );
+
+    if( dataFile && fileExists( fileName ) )
+    {
+        WLogger::getLogger()->addLogMessage( "opening file", "Read Amira Mesh", LL_DEBUG );
+    }
+    else
+    {
+        WLogger::getLogger()->addLogMessage( "open file failed" + fileName , "Read Amira Mesh", LL_ERROR );
+        return false;
+    }
+
+    std::string tmp;
+    getline( dataFile, tmp );
+    if( !checkVersionString( tmp ) )
+    {
+        return false;
+    }
+
+    skipEmptyAndCommentLines( dataFile, tmp );
+
+    std::vector< size_t > dimensions;
+    while( tmp.find( "define " ) == 0 )
+    {
+        dimensions.push_back( parseDefine( tmp ) );
+        getline( dataFile, tmp );
+    }
+
+    skipEmptyAndCommentLines( dataFile, tmp );
+
+    if( tmp.find( "Parameters {" ) == 0 )
+    {
+        if( !parseParameters( dataFile, tmp ) )
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+
+//#warning use information about defines
+    while( tmp.find( "@1" ) != 0 )
+    {
+        getline( dataFile, tmp );
+    }
+
+
+    WPosition vertex;
+    m_vertices.clear();
+    for( size_t vertexId = 0; vertexId < dimensions[0]; ++vertexId )
+    {
+        dataFile >> vertex[0] >> vertex[1] >> vertex[2];
+        m_vertices.push_back( vertex );
+    }
+
+
+
+//#warning use information about defines
+    while( tmp.find( "@2" ) != 0 )
+    {
+        getline( dataFile, tmp );
+    }
+
+
+    std::pair< size_t, size_t > edge;
+    m_edges.clear();
+    for( size_t edgeId = 0; edgeId < dimensions[1]; ++edgeId )
+    {
+        dataFile >> edge.first >> edge.second;
+        m_edges.push_back( edge );
+    }
+
+    return true;
+}
---
+++ src/modules/readAmiraMesh/WMReadAmiraMesh.h
@@ -0,0 +1,138 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV-Leipzig and CNCF-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WMREADAMIRAMESH_H
+#define WMREADAMIRAMESH_H
+
+#include <string>
+
+#include <osg/Geode>
+
+#include "core/dataHandler/WDataSetFibers.h"
+#include "core/kernel/WModule.h"
+#include "core/kernel/WModuleInputData.h"
+#include "core/kernel/WModuleOutputData.h"
+
+/**
+ * Someone should add some documentation here.
+ * Probably the best person would be the module's
+ * creator, i.e. "wiebel".
+ *
+ * This is only an empty template for a new module. For
+ * an example module containing many interesting concepts
+ * and extensive documentation have a look at "src/modules/template"
+ *
+ * \ingroup modules
+ */
+class WMReadAmiraMesh: public WModule
+{
+/**
+ * Only UnitTests may be friends.
+ */
+friend class WMReadAmiraMeshTest;
+
+public:
+
+    /**
+     *
+     */
+    WMReadAmiraMesh();
+
+    /**
+     *
+     */
+    virtual ~WMReadAmiraMesh();
+
+    /**
+     * Gives back the name of this module.
+     * \return the module's name.
+     */
+    virtual const std::string getName() const;
+
+    /**
+     * Gives back a description of this module.
+     * \return description to module.
+     */
+    virtual const std::string getDescription() const;
+
+    /**
+     * Due to the prototype design pattern used to build modules, this method returns a new instance of this method. NOTE: it
+     * should never be initialized or modified in some other way. A simple new instance is required.
+     *
+     * \return the prototype used to create every module in OpenWalnut.
+     */
+    virtual boost::shared_ptr< WModule > factory() const;
+
+    /**
+     * Get the icon for this module in XPM format.
+     *
+     * \return The icon.
+     */
+    virtual const char** getXPMIcon() const;
+
+protected:
+
+    /**
+     * Entry point after loading the module. Runs in separate thread.
+     */
+    virtual void moduleMain();
+
+    /**
+     * Initialize the connectors this module is using.
+     */
+    virtual void connectors();
+
+    /**
+     * Initialize the properties for this module.
+     */
+    virtual void properties();
+
+    /**
+     * Initialize requirements for this module.
+     */
+    virtual void requirements();
+
+
+private:
+    /**
+     * Very prelimiary and specialized way of reading a spatial graph from an amira mesh
+     */
+    bool readAmiraMesh( std::string fileName );
+
+    void prepareResult();
+
+    WPropFilename m_dataFile; //!< The data will be read from this file.
+    boost::shared_ptr< WCondition > m_propCondition;  //!< A condition used to notify about changes in several properties.
+
+    std::vector< WPosition > m_vertices; //!< vertices of the spatial graph
+    std::vector< std::pair< size_t, size_t > > m_edges; //!< Edge connectivity of the spatial graph.
+
+    /**
+     * The only output of this data module.
+     */
+    boost::shared_ptr< WModuleOutputData< WDataSetFibers > > m_output;
+    boost::shared_ptr< WDataSetFibers > m_graph;
+};
+
+#endif  // WMREADAMIRAMESH_H
