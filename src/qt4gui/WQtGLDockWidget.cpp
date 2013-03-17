1b5ae61aff1776a9accedc41bcda8a75d09b2e9e
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2013-03-17 18:22:42 +0100

    [FIX] crash fixed when starting with nav views enabled

--- src/qt4gui/WQtGLDockWidget.cpp
+++ src/qt4gui/WQtGLDockWidget.cpp
@@ -53,7 +53,7 @@ WQtGLDockWidget::WQtGLDockWidget( QString viewTitle, QString dockTitle, QWidget*
     // the panel contains all other widgets, including the gl widget
     // This allows adding other widgets to certain docks
     m_panel = new QWidget( this );
-    m_layout = new QVBoxLayout();
+    m_layout = new QVBoxLayout( m_panel );
     m_layout->setContentsMargins( 1, 1, 1, 1 );
 
     m_glWidget = boost::shared_ptr<WQtGLWidget>( new WQtGLWidget( viewTitle.toStdString(), m_panel, projectionMode, shareWidget ) );
