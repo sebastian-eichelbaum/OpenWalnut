2e8b11ca357102be317dc07f9477efd817b6b683
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2013-11-14 18:44:23 +0100

    [ADD #312] basic demo code for vignetting. Not yet working properly.

---
+++ src/core/graphicsEngine/shaders/shaders/WGECameraVignette-fragment.glsl
@@ -0,0 +1,48 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV-Leipzig and CNCF-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#version 120
+
+uniform float u_vignetteIntensity = 0.0;
+
+/**
+ * Pixel position in [0,1]
+ */
+varying vec2 v_pos;
+
+void main()
+{
+    vec3 color = vec3( 0.0, 0.0, 0.0 );
+    float alpha = 0.0;
+
+    // Basically, use a unit circle and shade alpha when nearing radius 1
+    vec2 posInUnitCircle = 2.0 * ( v_pos - 0.5 );
+    float xsqr = posInUnitCircle.x * posInUnitCircle.x;
+    float ysqr = posInUnitCircle.y * posInUnitCircle.y;
+
+    alpha = u_vignetteIntensity * ( xsqr * ysqr );
+
+    // use texture coordinate to mix along the cylinder
+    gl_FragColor = vec4( color, alpha );
+}
