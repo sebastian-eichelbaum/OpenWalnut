14af3ff867be23c77a82531853bd4a265493331f
Andreas Schwarzkopf <aschwarzkopf@openwalnut.org>
2014-05-05 13:39:48 +0200

    [ADD #359] Adding Principal Component Analysis to the core common  math folder  * Adds the Principal Component Analysis feature to src/core/common/math/PrincipalComponentAnalysis  * This algorithm analyzes the distribution of a selection of points. You'll get the main  * point distribution directions (Eigen Vectors) and variables how strongly points are distributed  * within that directions (Eigen Values)  * See the ticket of the issue ID #359 to get more information.

---
+++ src/core/common/math/PrincipalComponentAnalsis/WCovarianceSolver.cpp
@@ -0,0 +1,96 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV-Leipzig and CNCF-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#include <vector>
+#include "WCovarianceSolver.h"
+
+WCovarianceSolver::WCovarianceSolver()
+{
+}
+
+WCovarianceSolver::~WCovarianceSolver()
+{
+}
+void WCovarianceSolver::analyzeData( std::vector<WPosition> dataSet )
+{
+    m_dataSet = dataSet;
+    calculateMean();
+    calculateCovariance();
+}
+vector<double> WCovarianceSolver::getMean()
+{
+    return m_mean;
+}
+void WCovarianceSolver::calculateMean()
+{
+    if( m_dataSet.size() == 0 )
+        return;
+    size_t dimensions = m_dataSet[0].size();
+    m_mean.resize( dimensions );
+    m_mean.reserve( dimensions );
+    for( size_t dimension = 0; dimension < dimensions; dimension++ )
+        m_mean[dimension] = 0.0;
+    for( size_t index = 0; index < m_dataSet.size(); index++ )
+        for( size_t dimension = 0; dimension < m_dataSet[index].size(); dimension++ )
+            m_mean[dimension] += m_dataSet[index][dimension];
+    for( size_t dimension = 0; dimension < dimensions; dimension++ )
+        m_mean[dimension] /= static_cast<double>( m_dataSet.size() );
+}
+void WCovarianceSolver::calculateCovariance()
+{
+    size_t width = m_mean.size();
+    MatrixXd newCovariance( width, width );
+    m_covariance = newCovariance;
+    for( size_t row = 0; row < width; row++ )
+        for( size_t col = 0; col < width; col++ )
+            m_covariance( row, col ) = 0.0;
+    for( size_t index = 0; index < m_dataSet.size(); index++ )
+        addPointToCovariance( index );
+    for( size_t row = 0; row < width; row++ )
+        for( size_t col = 0; col < width; col++ )
+            m_covariance( row, col ) /= static_cast<double>( m_dataSet.size() ) - 1.0;
+}
+void WCovarianceSolver::addPointToCovariance( size_t pointIndex )
+{
+    size_t width = m_mean.size();
+    WPosition position = m_dataSet[pointIndex];
+    for( size_t row = 0; row < width; row++ )
+    {
+        double rowValue = position[row] - m_mean[row];
+        for( size_t col = 0; col < width; col++ )
+        {
+            double colValue = position[col] - m_mean[col];
+            m_covariance( row, col ) += rowValue * colValue;
+        }
+    }
+}
+MatrixXd WCovarianceSolver::getCovariance()
+{
+    return m_covariance;
+}
+void WCovarianceSolver::clearInputData()
+{
+    m_dataSet.resize( 0 );
+    m_dataSet.reserve( 0 );
+}
---
+++ src/core/common/math/PrincipalComponentAnalsis/WPrincipalComponentAnalysis.cpp
@@ -0,0 +1,109 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV-Leipzig and CNCF-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#include <vector>
+#include "WPrincipalComponentAnalysis.h"
+
+WPrincipalComponentAnalysis::WPrincipalComponentAnalysis()
+{
+    m_isValidPCA = false;
+}
+
+WPrincipalComponentAnalysis::~WPrincipalComponentAnalysis()
+{
+}
+
+void WPrincipalComponentAnalysis::analyzeData( vector<WPosition> inputData )
+{
+    m_isValidPCA = true;
+    m_inputData = inputData;
+    m_covarianceSolver.analyzeData( m_inputData );
+    vector<double> mean = m_covarianceSolver.getMean();
+    EigenSolver<MatrixXd> es( m_covarianceSolver.getCovariance() );
+    m_eigenSolver = es;
+    extractEigenData();
+    sortByVarianceDescending();
+    clearInputData();
+}
+void WPrincipalComponentAnalysis::extractEigenData()
+{
+    int count = m_eigenSolver.eigenvalues().size();
+    m_directions.reserve( count );
+    m_directions.resize( count );
+    m_eigenValues.reserve( count );
+    m_eigenValues.resize( count );
+    for( int index = 0; index < count; index++ )
+    {
+        complex<double> lambda = m_eigenSolver.eigenvalues()[index];
+        if( lambda.imag() != 0.0 )
+            m_isValidPCA = false;
+        m_eigenValues[index] = lambda.real();
+
+        VectorXcd vector = m_eigenSolver.eigenvectors().col( index );
+        WVector3d newPosition;
+        size_t vectorSize = static_cast<size_t>( vector.size() );
+        for( size_t dimension = 0; dimension < vectorSize && dimension < newPosition.size(); dimension++ )
+        {
+            complex<double> value = vector[dimension];
+            newPosition[dimension] = value.real();
+        }
+        m_directions[index] = newPosition;
+    }
+    sortByVarianceDescending();
+}
+void WPrincipalComponentAnalysis::sortByVarianceDescending()
+{
+    if( m_inputData.size() == 0 ) return;
+    size_t dimensions = m_inputData[0].size();
+    for( size_t d1 = 0; d1 < dimensions - 1; d1++ )
+        for( size_t d2 = d1 + 1; d2 < dimensions; d2++ )
+            if( m_eigenValues[d1] < m_eigenValues[d2] )
+                swapEigenVectors( d1, d2 );
+}
+void WPrincipalComponentAnalysis::swapEigenVectors( size_t eigenVectorIndex1, size_t eigenVectorIndex2 )
+{
+    for( size_t dimension = 0; dimension < m_eigenValues.size(); dimension++ )
+    {
+        double prevValue = m_directions[eigenVectorIndex1][dimension];
+        m_directions[eigenVectorIndex1][dimension] = m_directions[eigenVectorIndex2][dimension];
+        m_directions[eigenVectorIndex2][dimension] = prevValue;
+    }
+    double prevValue = m_eigenValues[eigenVectorIndex1];
+    m_eigenValues[eigenVectorIndex1] = m_eigenValues[eigenVectorIndex2];
+    m_eigenValues[eigenVectorIndex2] = prevValue;
+}
+vector<WVector3d> WPrincipalComponentAnalysis::getDirections()
+{
+    return m_directions;
+}
+vector<double> WPrincipalComponentAnalysis::getEigenValues()
+{
+    return m_eigenValues;
+}
+void WPrincipalComponentAnalysis::clearInputData()
+{
+    m_inputData.resize( 0 );
+    m_inputData.reserve( 0 );
+    m_covarianceSolver.clearInputData();
+}
