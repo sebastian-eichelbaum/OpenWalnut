f41b61ab2a8190e5f54a5a3dbaa7168654002873
Alexander Wiebel <alexander.wiebel@hs-coburg.de>
2015-05-27 21:12:13 +0200

    [ADD #391] skeleton for resampling module

---
+++ src/modules/resampleRegular/WMResampleRegular.cpp
@@ -0,0 +1,96 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2015 OpenWalnut Community, Nemtics, BSV@Uni-Leipzig
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#include <string>
+
+#include "core/kernel/WKernel.h"
+
+#include "WMResampleRegular.h"
+
+// This line is needed by the module loader to actually find your module. Do not remove. Do NOT add a ";" here.
+W_LOADABLE_MODULE( WMResampleRegular )
+
+WMResampleRegular::WMResampleRegular():
+    WModule()
+{
+}
+
+WMResampleRegular::~WMResampleRegular()
+{
+    // Cleanup!
+}
+
+boost::shared_ptr< WModule > WMResampleRegular::factory() const
+{
+    // See "src/modules/template/" for an extensively documented example.
+    return boost::shared_ptr< WModule >( new WMResampleRegular() );
+}
+
+const std::string WMResampleRegular::getName() const
+{
+    // Specify your module name here. This name must be UNIQUE!
+    return "Resample Regular";
+}
+
+const std::string WMResampleRegular::getDescription() const
+{
+    // Specify your module description here. Be detailed. This text is read by the user.
+    // See "src/modules/template/" for an extensively documented example.
+    return "No documentation yet. "
+    "The best person to ask for documenation is probably the modules's creator, i.e. the user with the name \"wiebel\".";
+}
+
+void WMResampleRegular::connectors()
+{
+    m_original = boost::shared_ptr<WModuleInputData<WDataSetScalar> >(
+        new WModuleInputData<WDataSetScalar> ( shared_from_this(), "Original",
+                                               "The dataset to resample." ) );
+    addConnector( m_original );
+
+    m_resampled = boost::shared_ptr<WModuleOutputData<WDataSetScalar> >(
+        new WModuleOutputData<WDataSetScalar> ( shared_from_this(), "Resampled",
+                                                "The resampled data set." ) );
+    addConnector( m_resampled );
+
+    WModule::connectors();
+}
+
+void WMResampleRegular::properties()
+{
+    // Put the code for your properties here. See "src/modules/template/" for an extensively documented example.
+
+    WModule::properties();
+}
+
+void WMResampleRegular::requirements()
+{
+    // Put the code for your requirements here. See "src/modules/template/" for an extensively documented example.
+}
+
+void WMResampleRegular::moduleMain()
+{
+    // signal ready state
+    ready();
+
+}
---
+++ src/modules/resampleRegular/WMResampleRegular.h
@@ -0,0 +1,109 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2015 OpenWalnut Community, Nemtics, BSV@Uni-Leipzig
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WMRESAMPLEREGULAR_H
+#define WMRESAMPLEREGULAR_H
+
+#include <string>
+
+#include <osg/Geode>
+
+#include "core/kernel/WModule.h"
+#include "core/kernel/WModuleInputData.h"
+#include "core/kernel/WModuleOutputData.h"
+
+#include "core/dataHandler/WDataSetScalar.h"
+
+/**
+ * Someone should add some documentation here.
+ * Probably the best person would be the module's
+ * creator, i.e. "wiebel".
+ *
+ * This is only an empty template for a new module. For
+ * an example module containing many interesting concepts
+ * and extensive documentation have a look at "src/modules/template"
+ *
+ * \ingroup modules
+ */
+class WMResampleRegular: public WModule
+{
+public:
+    /**
+     *
+     */
+    WMResampleRegular();
+
+    /**
+     *
+     */
+    virtual ~WMResampleRegular();
+
+    /**
+     * Gives back the name of this module.
+     * \return the module's name.
+     */
+    virtual const std::string getName() const;
+
+    /**
+     * Gives back a description of this module.
+     * \return description to module.
+     */
+    virtual const std::string getDescription() const;
+
+    /**
+     * Due to the prototype design pattern used to build modules, this method returns a new instance of this method. NOTE: it
+     * should never be initialized or modified in some other way. A simple new instance is required.
+     *
+     * \return the prototype used to create every module in OpenWalnut.
+     */
+    virtual boost::shared_ptr< WModule > factory() const;
+
+protected:
+    /**
+     * Entry point after loading the module. Runs in separate thread.
+     */
+    virtual void moduleMain();
+
+    /**
+     * Initialize the connectors this module is using.
+     */
+    virtual void connectors();
+
+    /**
+     * Initialize the properties for this module.
+     */
+    virtual void properties();
+
+    /**
+     * Initialize requirements for this module.
+     */
+    virtual void requirements();
+
+
+private:
+    boost::shared_ptr< WModuleInputData< WDataSetScalar > > m_original;  //!< Connector providing original input data.
+    boost::shared_ptr< WModuleOutputData< WDataSetScalar > > m_resampled; //!< Connector yielding resampled data.
+};
+
+#endif  // WMRESAMPLEREGULAR_H
